ee330dd5ab3e99fae716fd48ac1108e8b1bcf595
Lionel Dricot <ploum@spoutnik>
2008-10-28 21:05:27 +0100

    se ferme correctement ç

--- textview.py
+++ textview.py
@@ -15,10 +15,16 @@ except:
 
 class TaskEditor :
 	def __init__(self) :
-		window = gtk.Window(type=gtk.WINDOW_TOPLEVEL)
-		textview = gtk.TextView(buffer=None)
-		window.add(textview)
-		window.show_all()
+		self.window = gtk.Window(type=gtk.WINDOW_TOPLEVEL)
+		self.window.set_default_size(150,150)
+		self.textview = gtk.TextView(buffer=None)
+		self.window.add(self.textview)
+		self.window.connect("destroy", self.close)
+		self.window.show_all()
+		
+	def close(self,window) :
+		print self.textview.get_buffer()
+		gtk.main_quit()
 
 	
 
