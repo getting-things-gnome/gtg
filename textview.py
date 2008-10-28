d866fbda0cf52eaf73be1781536a053a33dbb906
Lionel Dricot <ploum@spoutnik>
2008-10-28 21:36:17 +0100

    on commence à importer

--- textview.py
+++ textview.py
@@ -1,4 +1,4 @@
-import sys, time, os
+import sys, time, os, xml.dom.minidom
 import string, threading
 
 try:
@@ -12,11 +12,32 @@ try:
 	import gobject
 except:
 	sys.exit(1)
+	
+zefile = "mynote.xml"
 
 class TaskEditor :
 	def __init__(self) :
 		self.window = gtk.Window(type=gtk.WINDOW_TOPLEVEL)
 		self.window.set_default_size(150,150)
+		#We open the note file or create it if it doesn't exist
+		if os.path.exists(zefile) :
+			f = open(zefile,mode='r')
+			# sanitize the pretty XML
+			stringed = f.read().replace('\n','').replace('\t','')
+			try :
+				doc = xml.dom.minidom.parseString(stringed)
+			except :
+				return 0
+		#the file didn't exist, create it now
+		else :
+			doc = xml.dom.minidom.Document()
+			content = doc.createElement("task")
+			doc.appendChild(content)
+			#then we create the file
+			f = open(zefile, mode='a+')
+			f.write(doc.toxml().encode("utf-8"))
+			f.close()
+		
 		self.textview = gtk.TextView(buffer=None)
 		self.window.add(self.textview)
 		self.window.connect("destroy", self.close)
@@ -27,12 +48,11 @@ class TaskEditor :
 		buff = self.textview.get_buffer()
 		#the tag table
 		table = buff.get_tag_table()
-		#We need two iterators to get the text
-		start = buff.get_start_iter()
-		end = buff.get_end_iter()
 		#we get the text
-		texte = buff.get_text(start,end)
-		print table
+		texte = buff.get_text(buff.get_start_iter(),buff.get_end_iter())
+		#We should have a look at Tomboy Serialize function 
+		#NoteBuffer.cs : line 1163
+		#Currently, we are not saving the tag table.
 		print texte
 		
 	def close(self,window) :
