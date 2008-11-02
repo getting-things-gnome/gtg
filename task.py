0d73145ad9720c4783407b64e6d178492abcf66f
Lionel Dricot <ploum@spoutnik>
2008-11-02 19:34:47 +0100

    will it work ?

--- task.py
+++ task.py
@@ -3,6 +3,8 @@ import string, threading
 #subfolders are added to the path
 sys.path[1:1]=["backends"]
 
+from xml_backend import Backend
+
 #This class represent a project : a list of tasks sharing the same backend
 class Project :
 	def __init__(self, name) :
@@ -13,23 +15,25 @@ class Project :
 	def list_tasks(self):
 		print "implement list_tasks in task.py"
 		
-	def get_task(self,id) :
-		print "implement get_task"
+	def get_task(self,ze_id) :
+		return self.backend.get_task(ze_id)
 		
-	def __getbackend(name) :
-		#currently not much done
+	def __getbackend(self,name) :
+		#currently not much done but the backend should depend on the name
 		return Backend()
 		
 
 #This class represent a task in GTG.
 class Task :
-	def __init__(self) :
-		pass
+	def __init__(self, ze_id) :
+		#the id of this task in the project
+		self.tid = ze_id
+		self.content
 		
 	def get_text(self) :
-		print "implement get_text"
+		return self.content
 		
 	def set_text(self,texte) :
-		print "implement set_text"
+		self.content = texte
 		
 	
--- test_task_editor.py
+++ test_task_editor.py
@@ -1,16 +1,11 @@
 import sys, time, os
 import string, threading
-#subfolders are added to the path
-sys.path[1:1]=["backends"]
 
 from task import Task,Project
 from taskeditor import TaskEditor
-from xml_backend import Backend
 
 if __name__ == "__main__":
-	gobject.threads_init()
-	backend = Backend() 
-	zeproject = Project(zebackend)
+	zeproject = Project("my_project")
 	# "1" is just the ID of the task
 	zetask = zeproject.get_task(1)
 	tv = TaskEditor(zetask)
