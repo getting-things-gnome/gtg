936dc2564989ceec86bfc7dc01e095443112d8d4
Ploum <ploum@nout>
2009-01-22 17:56:09 +0100

    fixing a bug when deleting

--- gtg_core/task.py
+++ gtg_core/task.py
@@ -280,8 +280,7 @@ class Task :
         for i in self.get_parents() :
             task = self.req.get_task(i)
             task.remove_subtask(self.get_id())
-        for j in self.get_subtasks() :
-            task = self.req.get_task(j)
+        for task in self.get_subtasks() :
             task.remove_parent(self.get_id())
         #then we remove effectively the task
         #self.req.delete_task(self.get_id())
