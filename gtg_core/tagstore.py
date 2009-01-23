fd303c52e11b8582987a860b0e6def4d88137231
Lionel Dricot <ploum@ploum.net>
2009-01-23 23:23:29 +0100

    tagstore is saved when modified, bug #320235

--- gtg_core/tagstore.py
+++ gtg_core/tagstore.py
@@ -30,11 +30,11 @@ class TagStore :
         #Their name doesn't begin with "@"
         
         #Build the "all tags tag"
-        self.alltag_tag = Tag("alltags_tag")
+        self.alltag_tag = Tag("alltags_tag",save_cllbk=self.save)
         self.alltag_tag.set_attribute("special","all")
         self.alltag_tag.set_attribute("icon","data/16x16/icons/tags_alltasks.png")
         #Build the "without tag tag"
-        self.notag_tag = Tag("notag_tag")
+        self.notag_tag = Tag("notag_tag",save_cllbk=self.save)
         self.notag_tag.set_attribute("special","notag")
         self.notag_tag.set_attribute("icon","data/16x16/icons/tags_notag.png")
             
@@ -44,7 +44,7 @@ class TagStore :
     def new_tag(self,tagname) :
         #we create a new tag from a name
         if not self.store.has_key(tagname) :
-            tag = Tag(tagname)
+            tag = Tag(tagname,save_cllbk=self.save)
             self.add_tag(tag)
             return tag
         else :
@@ -119,17 +119,22 @@ class TagStore :
 #A tag is defined by its name (in most cases, it will be "@something") and it can have multiple attributes
 class Tag :
 
-    def __init__(self,name) :
+    def __init__(self,name,save_cllbk=None) :
         self.attributes = {}
+        self.name = name
         self.set_attribute("name",name)
+        self.save = save_cllbk
         
     def get_name(self) :
         return self.get_attribute("name")
         
     def set_attribute(self,att_name,att_value) :
-        #FIXME : we have to be careful when changing the name
-        #if att_name == "name" :   
-        self.attributes[att_name] = att_value
+        #warning : only the constructor can set the "name"  
+        if att_name != "name" :
+            self.attributes[att_name] = att_value
+            self.save()
+        elif self.name == att_value :
+            self.attributes[att_name] = att_value
         
     def get_attribute(self,att_name) :
         if self.attributes.has_key(att_name) :
