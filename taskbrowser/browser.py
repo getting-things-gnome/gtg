68ed889cb0c90ba0060f5ca01cd508c341048e55
Lionel Dricot <ploum@ploum.net>
2009-01-25 00:15:56 +0100

    refactoring the colors

--- taskbrowser/browser.py
+++ taskbrowser/browser.py
@@ -10,6 +10,7 @@ import threading
 from taskeditor.editor       import TaskEditor
 from taskbrowser.CellRendererTags import CellRendererTags
 from taskbrowser import GnomeConfig
+from tools import colors
 
 #=== OBJECTS ===================================================================
 
@@ -262,7 +263,7 @@ class TaskBrowser:
 
     #If a task asked for the refresh, we don't refresh it to avoid a loop
     def refresh_tb(self,fromtask=None):
-#        print "refresh called"
+        print "refresh called"
         self.refresh_list()
         self.refresh_tags()
         #Refreshing the opened editors
@@ -444,29 +445,7 @@ class TaskBrowser:
         left_str    = task.get_days_left()
         tags        = task.get_tags()
         
-        # Compute color
-        my_color    = None
-        color_count = 0.0
-        color_dict  = {"red":0,"green":0,"blue":0}
-        for my_tag in tags:
-            my_color_str = my_tag.get_attribute("color")
-            if my_color_str :
-                my_color    = gtk.gdk.color_parse(my_color_str)
-                color_count = color_count + 1
-                color_dict["red"]   = color_dict["red"]   + my_color.red
-                color_dict["green"] = color_dict["green"] + my_color.green
-                color_dict["blue"]  = color_dict["blue"]  + my_color.blue
-        if color_count != 0:
-            red        = int(color_dict["red"]   / color_count)
-            green      = int(color_dict["green"] / color_count)
-            blue       = int(color_dict["blue"]  / color_count)
-            brightness = (red+green+blue) / 3.0
-            while brightness < 60000:
-                red        = int( (red   + 65535) / 2)
-                green      = int( (green + 65535) / 2)
-                blue       = int( (blue  + 65535) / 2)
-                brightness = (red+green+blue) / 3.0
-            my_color = gtk.gdk.Color(red, green, blue).to_string()
+        my_color = colors.background_color(tags)
 
         if treeview and not parent and len(task.get_subtasks()) == 0:
             itera = tree_store.get_iter_first()
