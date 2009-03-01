306a90d19a73afbd3c802e3520c744467345d2e7
Lionel Dricot <ploum@ploum.net>
2009-03-01 15:09:12 +0100

    adding the GPL3 header to all files

--- GTG/__init__.py
+++ GTG/__init__.py
@@ -1,7 +1,24 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Gettings Things Gnome! - a personnal organizer for the GNOME desktop
+# Copyright (c) 2008-2009 - Lionel Dricot & Bertrand Rousseau
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 import os
 
-URL             = "http://gtg.frispeech.com"
+URL             = "http://gtg.fritalk.com"
 EMAIL           = "gtg@lists.launchpad.net"
 VERSION         = '0.1'
 DIRECTORY       = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) 
