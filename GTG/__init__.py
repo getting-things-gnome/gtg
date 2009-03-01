acd5b76114885f27f654ae74f82d9749a43e7cb9
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2009-03-01 12:10:48 +0100

    Oops, forgot some files

---
+++ GTG/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+import os
+
+URL             = "http://gtg.frispeech.com"
+EMAIL           = "gtg@lists.launchpad.net"
+VERSION         = '0.1'
+DIRECTORY       = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) 
+SHARED_DATA_DIR = os.path.join(DIRECTORY, "data") 
+LOCALE_DIR      = os.path.join(DIRECTORY, "po") 
+ 
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+import glob
+import GTG
+
+author = 'The GTG Team'
+
+setup(
+  name         = 'GTG',
+  version      = GTG.VERSION,
+  url          = GTG.URL,
+  author       = author,
+  author_email = GTG.EMAIL,
+  description  = 'GTG is a personal organizer for the GNOME desktop environment.',
+  packages     = ['GTG','GTG.backends','GTG.core','GTG.taskbrowser','GTG.taskeditor','GTG.tools'],
+  package_data = {'GTG.taskbrowser':['GTG/taskbrowser/taskbrowser.glade'], 'GTG.taskeditor':['GTG/taskeditor/taskeditor.glade']},
+  data_files   = [
+    ('/usr/share/gtg', ['data/icons','data/firstrun_tasks.xml']),
+                 ],
+  scripts=['gtg',],
+)
