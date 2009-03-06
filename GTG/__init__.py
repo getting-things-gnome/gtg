de22761a1a4e61124af5463ddb79dfccc91a40ea
Lionel Dricot <ploum@ploum.net>
2009-03-07 00:15:46 +0100

    0.1 release

--- GTG/__init__.py
+++ GTG/__init__.py
@@ -20,12 +20,11 @@ import os
 
 URL             = "http://gtg.fritalk.com"
 EMAIL           = "gtg@lists.launchpad.net"
-VERSION         = '0.0.9rc3'
+VERSION         = '0.1'
 LOCAL_ROOTDIR   = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) 
 DIST_ROOTDIR    = "/usr/share/gtg"
 
 if not os.path.isdir( os.path.join(LOCAL_ROOTDIR,'data') ) :
-    #DATA_DIR = os.path.join(DIST_ROOTDIR,'data')
     DATA_DIR = DIST_ROOTDIR
 else:
     DATA_DIR = os.path.join(LOCAL_ROOTDIR,'data')
