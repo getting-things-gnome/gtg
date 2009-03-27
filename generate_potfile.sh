68f1b1ac99f3099bf6da2b4a3fabd6d9e6d21069
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2009-03-28 00:09:50 +0100

    Include glade file for .pot generation

--- generate_potfile.sh
+++ generate_potfile.sh
@@ -2,4 +2,5 @@
 POTFILE="locales/gtg.pot"
 rm $POTFILE
 touch $POTFILE
-find GTG/ -iname "*.py" -exec xgettext -j --language=Python --keyword=_ --output=locales/gtg.pot {} \;
+find GTG/ -iname "*.glade" -exec intltool-extract --type=gettext/glade {} \;
+find GTG/ \( -iname "*.py" -o -iname "*.glade.h" \) -exec xgettext -j --language=Python --keyword=_ --keyword=N_ --output=locales/gtg.pot {} \;
