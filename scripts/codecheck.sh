137cb4c193ee2b2287c92c94e914b9563a8d95db
Lionel Dricot <ploum@ploum.net>
2009-03-31 13:04:14 +0200

    codecheck

--- scripts/codecheck.sh
+++ scripts/codecheck.sh
@@ -34,12 +34,14 @@
 #C0321 : more than one statement on the same line (is that bad ?)
 #W0401 : * wildcard import (yes, we use them)
 #W0142 : use of * and ** arguments (yes, I find that handy)
+#I0011 : Locally disabling. We don't care if we disabled locally
 
 #pylint argument :
-disabled="C0324,C0103,C0301,C0111,R0914,R0903,R0915,R0904,R0912,R0201,R0913,C0323,R0902,W0102,W0232,W0105,C0321,W0401,W0142"
+disabled="C0324,C0103,C0301,C0111,R0914,R0903,R0915,R0904,R0912,R0201,R0913,C0323,R0902,W0102,W0232,W0105,C0321,W0401,W0142,I0011"
 args="--rcfile=/dev/null --include-ids=y --reports=n"
-grepped_out="Locally disabling"
-grep_out2="Undefined variable \'\_\'"
+#grepped_out="Locally disabling"
+#pylint doesn't recognize gettext _()
+grepped_out="Undefined variable '_'"
 
 echo "Running pychecker"
 echo "#################"
@@ -52,7 +54,7 @@ pyflakes GTG|grep -v "import \*"|grep -v "undefined name '_'"
 
 echo "Running pylint"
 echo "#################"
-pylint --rcfile=/dev/null --include-ids=y --reports=n --disable-msg=$disabled GTG/gtg.py|grep -v "$grepped_out"|grep -v "$grep_out2"
+pylint --rcfile=/dev/null --include-ids=y --reports=n --disable-msg=$disabled GTG/gtg.py|grep -v "$grepped_out"
 for i in GTG/*; do
 	if test -d $i && [ $i != "data" ]; then
 		#echo $i
