70ee9a988cc9892071c9996ffeafa401762e67d7
Lionel Dricot <ploum@ploum.net>
2009-04-01 11:56:19 +0200

    removing firstrun_Task.xml

--- GTG/core/__init__.py
+++ GTG/core/__init__.py
@@ -62,7 +62,6 @@ class CoreConfig:
     CONF_DIR = os.path.join(xdg_config_home,'gtg/')
     CONF_FILE = "gtg.conf"
     conf_dict = None
-    FIRSTRUN_FILE = "firstrun_tasks.xml"
 
     def __init__(self):
         if not os.path.exists(self.CONF_DIR) :
@@ -132,7 +131,6 @@ class CoreConfig:
                     if xp.hasAttribute(key) :
                         b[key] = str(xp.getAttribute(key))
             if firstrun :
-                #frf = os.path.join(GTG.DATA_DIR,self.FIRSTRUN_FILE)
                 frx = firstrun_tasks.populate()
                 back = classobj.Backend(b,firstrunxml=frx)
             else :
