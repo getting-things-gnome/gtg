4ff54c33d1ac95b35e9c7627844889375cb76d4f
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2009-04-03 22:32:44 +0200

    Correct man path

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def create_data_files():
     data_files.append(('share/icons/hicolor/scalable/apps', ['data/icons/hicolor/scalable/apps/gtg.svg']))
     # misc
     data_files.append(('share/applications', ['gtg.desktop']))
-    data_files.append(('man/man1', ['doc/gtg.1']))
+    data_files.append(('share/man/man1', ['doc/gtg.1']))
     return data_files
     
     
