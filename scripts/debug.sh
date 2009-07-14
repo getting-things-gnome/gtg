6baa23f2c1861e90e45fd6e2ce449bf40b5f3554
Jonathan Lange <jml@canonical.com>
2009-07-14 21:40:41 +1000

    Store debug data in folder clearly labelled as such. Make sure Bazaar ignores this folder.

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export XDG_DATA_HOME="./tests/xdg/data"
-export XDG_CONFIG_HOME="./tests/xdg/config"
+export XDG_DATA_HOME="./debug_data/xdg/data"
+export XDG_CONFIG_HOME="./debug_data/xdg/config"
 ./gtg
