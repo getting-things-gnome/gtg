80593fac57ae58f01802ea4f52998747a175179c
Paulo Cabido <paulo.cabido@gmail.com>
2009-07-25 19:58:39 +0100

    changes to the geolocalized plugin's structure

--- GTG/plugins/geolocalized-tasks/geolocalized-tasks.py
+++ GTG/plugins/geolocalized-tasks/geolocalized-tasks.py
@@ -1,13 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 - Paulo Cabido <paulo.cabido@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import gtk, pygtk
-import os
+import os, sys
 
 import Geoclue
 
-import clutter
-import cluttergtk
-import gobject
-import champlain
-import champlaingtk
+import clutter, cluttergtk
+import champlain, champlaingtk 
+
+# IMPORTANT This add's the plugin path to python sys path
+sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)))
+from marker import MarkerLayer
 
 
 from GTG.core.plugins.engine import PluginEngine
@@ -71,6 +88,7 @@ class geolocalizedTasks:
     def on_geolocalized_preferences(self, widget):
         pass
     
+    #=== SET TASK LOCATION =========================================================
     def set_task_location(self, widget, plugin_api, location=None):
         location = self.geoclue.get_location_info()
         self.plugin_api = plugin_api
@@ -254,6 +272,9 @@ class geolocalizedTasks:
         (latitude, longitude) = view.get_coords_at(int(event.x), int(event.y))
         self.marker_list[0].set_position(latitude, longitude)
     
+    #=== SET TASK LOCATION =========================================================
+        
+    #=== TAG VIEW CONTEXT MENU =====================================================
     def on_contextmenu_tag_location(self, widget, plugin_api):
         location = self.geoclue.get_location_info()
         self.plugin_api_context = plugin_api
@@ -352,6 +373,8 @@ class geolocalizedTasks:
         else:
             dialog.destroy()
     
+    #=== TAG VIEW CONTEXT MENU =====================================================
+    
     def zoom_in(self, widget, view):
         view.zoom_in()
 
@@ -384,33 +407,3 @@ class geolocalizedTasks:
          dialog.realize()
          dialog.run()
          dialog.destroy()
-
-
-
-class MarkerLayer(champlain.Layer):
-
-    def __init__(self):
-        champlain.Layer.__init__(self)
-        # a marker can also be set in RGB with ints
-        self.gray = clutter.Color(51, 51, 51)
-        
-        #RGBA
-        self.white = clutter.Color(0xff, 0xff, 0xff, 0xff)
-        self.black = clutter.Color(0x00, 0x00, 0x00, 0xff)
-        
-        self.hide()
-        
-    def add_marker(self, text, latitude, longitude, bg_color=None, text_color=None, font="Airmole 8"):
-        if not text_color:
-            text_color = self.white
-            
-        if not bg_color:
-            bg_color = self.gray
-        
-        marker = champlain.marker_new_with_text(text, font, text_color, bg_color)
-
-        #marker.set_position(38.575935, -7.921326)
-        if latitude and longitude:
-            marker.set_position(latitude, longitude)
-        self.add(marker)
-        return marker
