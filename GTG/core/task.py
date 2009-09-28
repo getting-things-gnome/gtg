fbc06a38240086727c39b641c82efe42258f4e4c
Mikkel Kjær Jensen <mikkel@mikkel-laptop>
2009-09-28 11:52:42 +0200

    more tidying up

--- GTG/core/task.py
+++ GTG/core/task.py
@@ -34,7 +34,7 @@ class Task:
     STA_ACTIVE    = "Active"
     STA_DISMISSED = "Dismiss"
     STA_DONE      = "Done"
-    
+
     def __init__(self, ze_id, requester, newtask=False):
         #the id of this task in the project should be set
         #tid is a string ! (we have to choose a type and stick to it)
@@ -481,7 +481,6 @@ class Task:
             task.remove_subtask(self.get_id())
         for task in self.get_subtasks():
             self.req.delete_task(task.get_id())
-            
         #then we remove effectively the task
         #self.req.delete_task(self.get_id())
 
