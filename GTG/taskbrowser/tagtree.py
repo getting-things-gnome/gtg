df17f2c98a840f58d98d2e02eedbd7ede248df3f
Kevin Mehall <km@kevinmehall.net>
2009-11-22 11:25:11 -0700

    prevent a tag from being reparented onto one of its children

--- GTG/taskbrowser/tagtree.py
+++ GTG/taskbrowser/tagtree.py
@@ -228,11 +228,15 @@ class TagTreeModel(gtk.GenericTreeModel):
             new_par_tag = self.tree.root
         
         # prevent illegal moves
-        if child_tag is new_par_tag: return
-        if new_par_tag is not self.tree.root:
-        	if new_par_tag.get_name()[0]!='@': return
-        if child_tag.get_name()[0]!='@': return
+        c = new_par_tag
+        while c is not self.tree.root:
+            if c is child_tag: return
+            c = c.get_parent()
         
+        if new_par_tag is not self.tree.root:
+            if new_par_tag.get_name()[0]!='@': return
+        if child_tag.get_name()[0]!='@': return
+
         child_tag.reparent(new_par_tag)
 
         # Warn tree about deleted row
