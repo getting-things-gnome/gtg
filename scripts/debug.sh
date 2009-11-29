c7afc0e4bc42e22de5f5b8c67c34b8c18ebc4962
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2009-11-30 00:19:59 +0100

    Created and added a test data to build up a more representative dataset. Debug script should be modified in order to allow to select a specific dataset

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export XDG_DATA_HOME="./debug_data/xdg/data"
-export XDG_CACHE_HOME="./debug_data/xdg/cache"
-export XDG_CONFIG_HOME="./debug_data/xdg/config"
+mkdir -p tmp
+cp -r test/data/standard tmp/
+export XDG_DATA_HOME="./tmp/standard/xdg/data"
+export XDG_CACHE_HOME="./tmp/standard/xdg/cache"
+export XDG_CONFIG_HOME="./tmp/standard/xdg/config"
 ./gtg -d
