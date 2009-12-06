eb8165a7178cc895b5209a456ab03bb1562c20f6
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2009-12-06 14:30:23 +0100

    Further improvement to debug script: allow to specify debug output "-d" and dataset "-s <dataset>"

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
+
+args=""
+set="default"
+
 # Create execution-time data directory if needed
 mkdir -p tmp
+
 # Interpret arguments
-if [$# -gt 0] && [! $1 : '-d']
+while getopts ds: o
+do  case "$o" in
+    d)   args="-d";;
+    s)   set="$OPTARG";;
+    [?]) echo >&2 "Usage: $0 [-s dataset] [-d]"
+         exit 1;;
+    esac
+done
+
+# Copy dataset
+if [  $set != "default" ]
 then
-    if [ ! -d "./tmp/$1" ]
+    if [ ! -d "./tmp/$set" ]
     then
-        echo "Copying $1 dataset to ./tmp/"
-        cp -r test/data/$1 tmp/
+        echo "Copying $set dataset to ./tmp/"
+        cp -r test/data/$set tmp/
     fi
-    echo "Setting XDG vars accordingly."
-    export XDG_DATA_HOME="./tmp/$1/xdg/data"
-    export XDG_CACHE_HOME="./tmp/$1/xdg/cache"
-    export XDG_CONFIG_HOME="./tmp/$1/xdg/config"
+    echo "Setting XDG vars to use $set dataset."
+    export XDG_DATA_HOME="./tmp/$set/xdg/data"
+    export XDG_CACHE_HOME="./tmp/$set/xdg/cache"
+    export XDG_CONFIG_HOME="./tmp/$set/xdg/config"
 else
-    echo "Setting XDG vars to default value."
+    echo "Setting XDG vars to use default dataset."
     export XDG_DATA_HOME="./tmp/default/xdg/data"
     export XDG_CACHE_HOME="./tmp/default/xdg/cache"
     export XDG_CONFIG_HOME="./tmp/default/xdg/config"
 fi
-./gtg
+
+./gtg $args
