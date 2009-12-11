f9a7c085378f7bdb57cc82336227584f5110582b
Lionel Dricot <ploum@ploum.net>
2009-12-11 15:57:14 +0100

    cleaning of dayselected_double

--- GTG/taskeditor/editor.py
+++ GTG/taskeditor/editor.py
@@ -91,7 +91,6 @@ class TaskEditor :
                 "on_set_fuzzydate_now"  : self.set_fuzzydate_now,
                 "on_set_fuzzydate_soon" : self.set_fuzzydate_soon,
                 "on_set_fuzzydate_later": self.set_fuzzydate_later,
-                #"on_dayselected_double" : self.day_selected_double,
         }
         self.builder.connect_signals(dic)
         self.window         = self.builder.get_object("TaskEditor")
@@ -414,9 +413,7 @@ class TaskEditor :
         self.calendar.connect('button-press-event', self.__focus_out)
         self.sigid = self.cal_widget.connect("day-selected",self.day_selected)
         self.sigid_month = self.cal_widget.connect("month-changed",self.month_changed)
-        
 
-        
     def day_selected(self,widget) :
         y,m,d = widget.get_date()
         if self.__opened_date == "due" :
@@ -424,8 +421,9 @@ class TaskEditor :
         elif self.__opened_date == "start" :
             self.task.set_start_date(dates.strtodate("%s-%s-%s"%(y,m+1,d)))
         if self.close_when_changed :
+            #When we select a day, we connect the mouse release to the
+            #closing of the calendar.
             self.mouse_sigid = self.cal_widget.connect('event',self.__mouse_release)
-            #self.__close_calendar()
         else :
             self.close_when_changed = True
         self.refresh_editor()
@@ -438,10 +436,7 @@ class TaskEditor :
     def month_changed(self,widget) :
         #This is a ugly hack to close the calendar on the first click
         self.close_when_changed = False
-    
-    def day_selected_double(self,widget) : #pylint: disable-msg=W0613
-        self.__close_calendar()
-        
+
     def set_opened_date(self, date):
         if self.__opened_date == "due" :
             self.task.set_due_date(date)
