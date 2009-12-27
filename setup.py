c4fe18a001d0bc5688989d0bc3230350f3d23902
Luca Invernizzi <invernizzi.l@gmail.com>
2009-12-27 12:18:31 +0100

    aa

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ setup(
                                       'icons/hicolor/svg/assign-location.svg',
                                       'icons/hicolor/svg/geolocalization.svg'],
     'GTG.plugins.helloworld':['hello_world.glade'],
-    'GTG.plugins.tomboy':['tomboy.ui']
+    'GTG.plugins.tomboy':['tomboy.ui'],
     'GTG.plugins.notification_area':['notification_area.ui']
     },
   data_files = create_data_files(),
