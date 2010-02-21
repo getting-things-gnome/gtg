dc919dd4e0f52e103e633a4412189766dbca718d
Lionel Dricot <ploum@ploum.net>
2010-02-21 17:12:04 +0100

    filter tree

---
+++ GTG/taskbrowser/filtertree.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Gettings Things Gnome! - a personal organizer for the GNOME desktop
+# Copyright (c) 2008-2009 - Lionel Dricot & Bertrand Rousseau
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+import gtk
+
+class TaskTreeModel(gtk.GenericTreeModel):
+
+    def __init__(self,tasktree):
+        gtk.GenericTreeModel.__init__(self)
+        self.tree = tasktree
+        
+    def on_get_flags(self):
+        return self.tree.on_get_flags()
+        
+    def on_get_n_columns(self):
+        return self.tree.on_get_n_columns()
+        
+    def on_get_column_type(self, index):
+        return self.tree.on_get_column_type(index)
+        
+    def on_get_iter(self, path):
+        return self.tree.on_get_iter(path)
+        
+    def on_get_path(self, rowref):
+        return self.tree.on_get_path(rowref)
+        
+    def on_get_value(self, rowref, column):
+        return self.tree.on_get_value(rowref,column)
+        
+    def on_iter_next(self, rowref):
+        return self.tree.on_iter_next(rowref)
+        
+    def on_iter_children(self, parent):
+        return on_iter_children(parent)
+        
+    def on_iter_has_child(self, rowref):
+        return self.tree.on_iter_has_child(rowref)
+        
+    def on_iter_n_children(self, rowref):
+        return self.tree.on_iter_n_children(rowref)
+        
+    def on_iter_nth_child(self, parent, n):
+        return self.tree.on_iter_nth_child(parent,n)
+        
+    def on_iter_parent(self, child)
+        return self.tree.on_iter_parent(child)
