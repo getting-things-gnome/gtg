14e95a8a6295accb6e026faaa3dad2bc10ee069d
David Planella <david.planella@ubuntu.com>
2010-03-12 09:51:57 +0100

    Updated the script to generate the binary catalogs

--- scripts/generate_mofile.sh
+++ scripts/generate_mofile.sh
@@ -2,9 +2,9 @@
 
 # TODO: We should find some way to automatically detect languages
 # by listing .po files but I'm lazy :-)
-LANGUAGES=$(ls locales/gtg-*.po | sed 's/locales\/gtg-\(.*\).po/\1/')
+LANGUAGES=$(ls po/*.po | sed 's/po\/\(.*\).po/\1/')
 
 for i in $LANGUAGES; do
-	mkdir locales/$i/LC_MESSAGES/ --parents
-	msgfmt locales/gtg-$i.po --output-file=locales/$i/LC_MESSAGES/gtg.mo
+	mkdir po/$i/LC_MESSAGES/ --parents
+	msgfmt po/$i.po --output-file=po/$i/LC_MESSAGES/gtg.mo
 done
