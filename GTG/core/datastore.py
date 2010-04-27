e621b56715f083fc8d2fc298cb9a529871fb1e4e
Bryce Harrington <bryce@canonical.com>
2010-04-26 17:11:47 -0700

    pydoc for DataStore class

--- GTG/core/datastore.py
+++ GTG/core/datastore.py
@@ -18,7 +18,7 @@
 # -----------------------------------------------------------------------------
 
 """
-datastore contains a list of "TagSource", which are proxies between a backend and the datastore itself
+datastore contains a list of "TagSource" objects, which are proxies between a backend and the datastore itself
 """
 
 import threading
@@ -40,6 +40,7 @@ THREADING = True
 class DataStore:
 
     def __init__(self):
+        """ Initializes a DataStore object """
         self.backends = {}
         self.open_tasks = Tree()
         self.closed_tasks = Tree()
@@ -47,12 +48,25 @@ class DataStore:
         self.tagstore = tagstore.TagStore(self.requester)
 
     def all_tasks(self):
+        """
+        @return: List of all keys of open tasks
+        """
         return self.open_tasks.get_all_keys()
 
     def has_task(self, tid):
+        """
+        @param tid: Task ID to search for
+        @return: True if the tid is among the open or closed tasks for
+        this DataStore, False otherwise.
+        """
         return self.open_tasks.has_node(tid) or self.closed_tasks.has_node(tid)
 
     def get_task(self, tid):
+        """
+        @param tid: Task ID to retrieve
+        @return: The internal task object for the given tid, or None if the
+         tid is not present in this DataStore.
+        """
         uid, pid = tid.split('@')
         if self.has_task(tid):
             task = self.__internal_get_task(tid)
@@ -72,6 +86,12 @@ class DataStore:
         return toreturn
 
     def delete_task(self, tid):
+        """
+        Deletes the given task entirely from this DataStore, and unlinks
+        it from the task's parent.
+        @return: True if task was deleted, or False if the tid was not
+        present in this DataStore.
+        """
         if tid and self.has_task(tid):
             self.__internal_get_task(tid).delete()
             uid, pid = tid.split('@') #pylint: disable-msg=W0612
@@ -86,6 +106,12 @@ class DataStore:
             
             
     def new_task(self,pid=None):
+        """
+        Creates a blank new task in this DataStore.
+        @param pid: (Optional) parent ID that this task should be a child of.
+         If not specified, the task will be a child of the default backend.
+        @return: The task object that was created.
+        """
         if not pid:
             pid = DEFAULT_BACKEND
         newtid = self.backends[pid].new_task_id()
@@ -104,9 +130,14 @@ class DataStore:
         return self.requester
         
     def get_tasks_tree(self):
+        """ @return: Open tasks tree """
         return self.open_tasks
         
     def push_task(self,task):
+        """
+        Adds the given task object as a node to the open tasks tree.
+        @param task: A valid task object
+        """
         tid = task.get_id()
         if self.has_task(tid):
             print "pushing an existing task. We should care about modifications"
@@ -117,6 +148,11 @@ class DataStore:
             task.set_sync_func(self.backends[pid].set_task,callsync=False)
     
     def task_factory(self,tid):
+        """
+        Instantiates the given task id as a Task object.
+        @param tid: The id of the task to instantiate
+        @return: The task object instantiated for tid
+        """
         task = None
         if self.has_task(tid):
             print "error : tid already exists"
@@ -126,6 +162,12 @@ class DataStore:
             
 
     def register_backend(self, dic):
+        """
+        Registers a TaskSource as a backend for this DataStore
+        @param dic: Dictionary object with a "backend" and "pid"
+         specified.  dic["pid"] should be the parent ID to use
+         with the backend specified in dic["backend"].
+        """
         if "backend" in dic:
             pid = dic["pid"]
             backend = dic["backend"]
@@ -139,20 +181,28 @@ class DataStore:
             print "Register a dic without backend key:  BUG"
 
     def unregister_backend(self, backend):
+        """ Unimplemented """
         print "unregister backend %s not implemented" %backend
 
     def get_all_backends(self):
+        """ @return: list of all registered backends for this DataStore """
         l = []
         for key in self.backends:
             l.append(self.backends[key])
         return l
 
-#Task source is an transparent interface between the real backend and datastore
-#Task source has also more functionnalities
-
 class TaskSource():
+    """
+    A transparent interface between the real backend and the datastore,
+    with additional functionality.
+    """
 
     def __init__(self, backend, parameters):
+        """
+        Instantiates a TaskSource object.
+        @param backend: (Required) Task Backend being wrapperized
+        @param parameters: Dictionary of custom parameters.
+        """
         self.backend = backend
         self.dic = parameters
         self.to_set = []
@@ -160,13 +210,21 @@ class TaskSource():
         self.lock = threading.Lock()
         self.count_set = 0
         
-    ### TaskSource/bakcend mapping
     def start_get_tasks(self,push_task,task_factory):
+        """
+        Maps the TaskSource to the backend and starts threading.
+        This must be called before the DataStore is usable.
+        """
         func = self.backend.start_get_tasks
         t = threading.Thread(target=func,args=(push_task,task_factory))
         t.start()
     
     def set_task(self, task):
+        """
+        Updates the task in the DataStore.  Actually, it adds the task to a
+        queue to be updated asynchronously.
+        @param task: The Task object to be updated.
+        """
         tid = task.get_id()
         if task not in self.to_set and tid not in self.to_remove:
             self.to_set.append(task)
@@ -178,6 +236,10 @@ class TaskSource():
 #            print "cannot acquire lock : not a problem, just for debug purpose"
             
     def setting_thread(self):
+        """
+        Operates the threads to set and remove tasks.
+        Releases the lock when it is done.
+        """
         try:
             while len(self.to_set) > 0:
                 t = self.to_set.pop(0)
@@ -193,6 +255,10 @@ class TaskSource():
             self.lock.release()
     
     def remove_task(self, tid):
+        """
+        Queues task to be removed.
+        @param tid: The Task ID of the task to be removed
+        """
         if tid not in self.to_remove:
             self.to_remove.append(tid)
         if self.lock.acquire(False):
@@ -201,11 +267,18 @@ class TaskSource():
             t.start()
     
     def new_task_id(self):
+        """
+        @return: A new ID created by the backend.
+        """
         return self.backend.new_task_id()
     
     def quit(self):
+        """ Quits the backend """
         self.backend.quit()
         
     #Those functions are only for TaskSource
     def get_parameters(self):
+        """
+        @return: The parameters specified during creation of the DataStore
+        """
         return self.dic
