0b5f0997c7eb0e4d39c502328c908f19d12a439f
Lionel Dricot <ploum@ploum.net>
2010-05-26 10:55:45 +0200

    small cleaning

--- GTG/core/datastore.py
+++ GTG/core/datastore.py
@@ -43,7 +43,7 @@ class DataStore:
         """ Initializes a DataStore object """
         self.backends = {}
         self.open_tasks = Tree()
-        self.closed_tasks = Tree()
+#        self.closed_tasks = Tree()
         self.requester = requester.Requester(self)
         self.tagstore = tagstore.TagStore(self.requester)
 
@@ -59,7 +59,7 @@ class DataStore:
         this DataStore, False otherwise.
         param tid: Task ID to search for
         """
-        return self.open_tasks.has_node(tid) or self.closed_tasks.has_node(tid)
+        return self.open_tasks.has_node(tid) #or self.closed_tasks.has_node(tid)
 
     def get_task(self, tid):
         """
@@ -79,14 +79,14 @@ class DataStore:
             return None
         
     def __internal_get_task(self, tid):
-        toreturn = self.open_tasks.get_node(tid)
-        if toreturn == None:
-            self.closed_tasks.get_node(tid)
+        return self.open_tasks.get_node(tid)
+#        if toreturn == None:
+#            self.closed_tasks.get_node(tid)
         #else:
             #print "error : this task doesn't exist in either tree"
             #pass
         #we return None if the task doesn't exist
-        return toreturn
+#        return toreturn
 
     def delete_task(self, tid):
         """
@@ -105,7 +105,7 @@ class DataStore:
         #by its parent a few line earlier :
         if self.has_task(tid):
             self.open_tasks.remove_node(tid)
-            self.closed_tasks.remove_node(tid)
+#            self.closed_tasks.remove_node(tid)
         back.remove_task(tid)
         return True
             
