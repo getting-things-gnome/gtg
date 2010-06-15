b7e91d5317c7cb8e48f6f1bfd33d9161da85004c
Lionel Dricot <ploum@ploum.net>
2010-06-15 10:01:10 +0200

    should fix closed tasks sorting bug #556159

--- GTG/gtk/browser/tasktree.py
+++ GTG/gtk/browser/tasktree.py
@@ -723,15 +723,3 @@ class ClosedTaskTreeView(TaskTreeView):
         self.set_search_column(COL_TITLE)
         
         self.set_show_expanders(False)
-
-    def scroll_to_task(self, task_id):
-        print "scroll to task does nothing : remove it"
-#        model = self.get_model()
-#        iter = model.get_iter_first()
-#        while iter:
-#            if model.get_value(iter, 1).get_id() == task_id:
-#                break
-#            iter = model.iter_next(iter)
-#        self.scroll_to_cell(model.get_path(iter),
-#                        self.tag_col,
-#                        False)
