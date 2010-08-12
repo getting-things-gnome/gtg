bf3f104ed5190a8b026ad50d6456c030d0777f54
Lionel Dricot <ploum@bastet>
2010-08-12 11:38:19 +0200

    mm its seems that sometimes, we are in a rather buggy state when sending the modified signal

--- GTG/gtk/liblarch_gtk/treemodel.py
+++ GTG/gtk/liblarch_gtk/treemodel.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
-DEBUG_MODEL = False
+DEBUG_MODEL = True
 TM_USE_SIGNALS = False
 TM_IDLE_ADD = False
 THREAD_PROTECTION = True
