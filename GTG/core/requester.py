82748a6c66bcb2ffc245dc92d0faaf8a85b48d7f
Lionel Dricot <ploum@ploum.net>
2011-08-16 16:14:05 +0200

    added a FIXME

--- GTG/core/requester.py
+++ GTG/core/requester.py
@@ -181,6 +181,7 @@ class Requester(gobject.GObject):
         @return: A list of tag names used by a task.
         """
         l = []
+        #FIXME: let's use another view instead of the activetags one
         view = self.ds.get_tagstore().get_viewtree(name='activetags')
         l = view.get_all_nodes()
         l.sort(cmp=lambda x, y: cmp(x.lower(),y.lower()))
