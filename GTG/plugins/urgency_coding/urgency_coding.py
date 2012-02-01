e5750f1ab864be645404a0a2469da8db1b14831d
Wolter Hellmund <wolterh6@gmail.com>
2012-02-01 16:54:54 -0600

    Task coloring works, preferences dialog is WIP

--- GTG/plugins/urgency_coding/urgency_coding.py
+++ GTG/plugins/urgency_coding/urgency_coding.py
@@ -14,25 +14,118 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from GTG.tools.dates import date, no_date
+from math import ceil
+
+import gtk
+
 class pluginUrgencyCoding:
+
+    PLUGIN_NAME = 'Urgency color'
+    # FIXME: Choose a definitive name for the plugin and make it
+    # consistent in all the plugin files.
+    DEFAULT_PREFERENCES = {
+        'reddays':30,
+        'color_red':'#cfff84',
+        'color_yellow':'#ffed84',
+        'color_green':'#ff9784'
+    }
     
     def __init__(self):
-        self.plugin_api = None
+        self._plugin_api = None
         self.req = None
 
     def activate(self, plugin_api):
         """ Plugin is activated """
-        self.plugin_api = plugin_api
-        self.req = self.plugin_api.get_requester()
+        self._plugin_api = plugin_api
+        self.req = self._plugin_api.get_requester()
+        self.prefs_load()
         # Set color function
-        self.plugin_api.set_bgcolor_func(self.bgcolor)
+        self._plugin_api.set_bgcolor_func(self.bgcolor)
 
     def bgcolor(self, node_id, standard_color):
         node = self.req.get_task(node_id)
-        print '%s => from %s until %s' % (node.get_title(), node.get_start_date(), node.get_due_date())
-        # Return color for this node
-        return '#000'
+        sdate = node.get_start_date()
+        ddate = node.get_due_date()
+        if (sdate != no_date != ddate):
+            dayspan = (ddate - sdate).days
+            reddays = ceil(3.0*dayspan/10)
+            daysleft = ddate.days_left()
 
-    def deactivate(self, plugin_api):
+            if daysleft == None \
+                    and ddate.__class__.__name__ != 'RealDate':
+                daysleft = (ddate.offset - date.today()).days
+            color = 0
+            if daysleft <= dayspan:
+                color = 1
+            if daysleft <= reddays:
+                color = 2
+            # This list should be implemented in the settings
+            colors = ['#cfff84','#ffed84','#ff9784']
+            #print "Giving color"
+            return colors[color]
+        else:
+            # Return color for this node
+            return None
+
+    def deactivate(self._plugin_api):
         """ Plugin is deactivated """
-        self.plugin_api.set_bgcolor_func()
+        self._plugin_api.set_bgcolor_func()
+
+# Preferences dialog
+
+    def configure_dialog(self, manager_dialog):
+        self.prefs_window.show_all()
+        self.prefs_window.set_transient_for(manager_dialog)
+
+    def prefs_init(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'preferences.ui'))
+        # Get the widgets
+        self.prefs_window = self.builder.get_object('window')
+        self.spinner_reddays = self.builder.get_object('spinner_reddays')
+        self.colorbutton_red = self.builder.get_object('colorbutton_red')
+        self.colorbutton_yellow = self.builder.get_object('colorbutton_yellow')
+        self.colorbutton_green = self.builder.get_object('colorbutton_green')
+        self.button_apply =  self.builder.get_objet('button_apply')
+        self.button_reset = self.builder.get_object('button_reset')
+        SIGNAL_CONNECTIONS_DIC = {
+            'on_window_close_event':
+                self.on_prefs_cancel,
+            'on_prefs_apply_event':
+                self.on_prefs_apply,
+            'on_prefs_reset_event':
+                self.on_prefs_reset,
+        }
+        self.builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
+
+    def on_prefs_cancel(self, widget=None, data=None):
+        self.window.hide()
+        # Restore widgets to current saved settings
+        return True
+
+    def on_prefs_apply(self, widget=None, data=None):
+        self.prefs_store()
+        self.window.hide()
+
+    def on_prefs_reset(self, widget=None, data=None):
+        # Restore the default plugin settings
+        self.window.hide()
+
+    def prefs_load(self):
+        data = self._plugin_api.load_configuration_object( \
+            self.PLUGIN_NAME,
+            'preferences')
+        if not data or not isinstance(data, dict):
+            self._pref_data = self.DEFAULT_PREFS
+        else:
+            self._pref_data = data
+
+    def prefs_store(self):
+        self._plugin_api.save_configuration_object( \
+            self.PLUGIN_NAME,
+            'preferences'
+            self._pref_data) #FIXME: need such an attribute
+
