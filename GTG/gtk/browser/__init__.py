f3808a7346c93dea3063d78bad9afe908fd962a2
Izidor Matušov <izidor.matusov@gmail.com>
2012-02-05 20:10:32 +0100

    Changed tooltips

--- GTG/gtk/browser/__init__.py
+++ GTG/gtk/browser/__init__.py
@@ -46,10 +46,5 @@ class GnomeConfig:
     WORKVIEW_TOGGLE_TOOLTIP = _("Display only the currently actionable tasks")
     TAG_IN_WORKVIEW_TOGG    = _("Hide this tag from the workview")
     TAG_NOTIN_WORKVIEW_TOGG = _("Show this tag in the workview")
-#FIXME SEARCH
-    SAVEASVIEW              = _("Save as View...")
-#FIXME
-    QUICKADD_ENTRY_TOOLTIP  = _("you can filter, create or open your tasks here\nSearch using ! for commands, @ for tags or dates")
-    QUICKADD_ENTRY_TOOLTIP  = _("Search help")
-#FIXME
+    QUICKADD_ENTRY_TOOLTIP  = _("You can create, open or filter your tasks here")
     QUICKADD_ICON_TOOLTIP   = _("Clear")
