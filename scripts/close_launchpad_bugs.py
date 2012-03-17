5c1ae7058c22d8b1a3231bc495e8aebbea430d72
Izidor Matušov <izidor.matusov@gmail.com>
2012-03-17 01:53:39 +0100

    Updated & cleaned scripts for building PPA + build_integrity.py

--- scripts/close_launchpad_bugs.py
+++ scripts/close_launchpad_bugs.py
@@ -18,32 +18,47 @@
 
 #documentation: https://edge.launchpad.net/+apidoc/#bug_task
 
-import sys, os, re
-from launchpadlib.launchpad import Launchpad, EDGE_SERVICE_ROOT
-from launchpadlib.credentials import Credentials
+import os
+import re
+import sys
 from re import findall
 from urllib import urlopen
 
+from launchpadlib.credentials import Credentials
+from launchpadlib.launchpad import Launchpad, EDGE_SERVICE_ROOT
+
+
 def lp_login():
-        cachedir = os.path.expanduser('~/.cache/launchpadlib/')
-        if not os.path.isdir(cachedir):
-                os.makedirs(cachedir)
-        creddir = os.path.expanduser("~/.cache/lp_credentials")
-        if not os.path.isdir(creddir):
-                os.makedirs(creddir)
-                os.chmod(creddir, 0700)
-        try:
-                credfile = open(os.path.join(creddir, 'close_launchpad_bugs.txt'), 'r')
-                credentials = Credentials()
-                credentials.load(credfile)
-                credfile.close()
-                launchpad = Launchpad(credentials, EDGE_SERVICE_ROOT, cachedir)
-        except IOError:
-                launchpad = Launchpad.get_token_and_login('close_launchpad_bugs', EDGE_SERVICE_ROOT, cachedir)
-                credfile = open(os.path.join(creddir, 'close_launchpad_bugs.txt'), 'w')
-                launchpad.credentials.save(credfile)
-                credfile.close()
-        return launchpad
+    cachedir = os.path.expanduser('~/.cache/launchpadlib/')
+    if not os.path.isdir(cachedir):
+        os.makedirs(cachedir)
+    creddir = os.path.expanduser("~/.cache/lp_credentials")
+    if not os.path.isdir(creddir):
+        os.makedirs(creddir)
+        os.chmod(creddir, 0700)
+
+    credpath = os.path.join(creddir, 'close_launchpad_bugs.txt')
+    try:
+        credfile = open(credpath, 'r')
+        credentials = Credentials()
+        credentials.load(credfile)
+        credfile.close()
+        launchpad = Launchpad(credentials, EDGE_SERVICE_ROOT, cachedir)
+    except IOError:
+        launchpad = Launchpad.get_token_and_login('close_launchpad_bugs',
+                                                EDGE_SERVICE_ROOT, cachedir)
+        credfile = open(credpath, 'w')
+        launchpad.credentials.save(credfile)
+        credfile.close()
+
+    return launchpad
+
+
+def process_bug(bug):
+    for task in bug.bug_tasks:
+        if task.bug_target_name == 'gtg' and task.status == 'Fix Committed':
+            task.status = 'Fix Released'
+            task.lp_save()
 
 if len(sys.argv) != 2:
     print 'Usage: %s <release>' % sys.argv[0]
@@ -52,20 +67,15 @@ if len(sys.argv) != 2:
 data = urlopen('https://launchpad.net/gtg/+milestone/%s' % sys.argv[1]).read()
 bugs = findall('<a href="\S+/bugs/(\d+)">', data)
 launchpad = lp_login()
+
 if not 'gtg' in [e.name for e in launchpad.people[launchpad.me].super_teams]:
     print 'You are not a GTG developer, exiting.'
     sys.exit(0)
+
 for bugno in bugs:
     try:
-        bug = launchpad.bugs[bugno]
-        for task in bug.bug_tasks:
-            if task.bug_target_name == 'gtg' and task.status == 'Fix Committed':
-                task.status = 'Fix Released'
-                #task.milestone_link = 'https://api.edge.launchpad.net/beta/gtg/+milestone/0.2.2'
-                task.lp_save()
-                print """Bug %s marked as Fix Released,
-                    (https://bugs.edge.launchpad.net/gtg/+bug/%s""" % (bugno, bugno)
-
+        process_bug(launchpad.bugs[bugno])
+        print "Bug #%s marked as Fix Released: " \
+            "https://bugs.edge.launchpad.net/gtg/+bug/%s" % (bugno, bugno)
     except:
-        print 'UNABLE TO PROCESS BUG #%s' % bugno
-        pass
+        print "UNABLE TO PROCESS BUG #%s" % bugno
