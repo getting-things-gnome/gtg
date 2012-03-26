58e364d77e0f76a0a9caf1381a9490ce66790332
Anant Gupta <anant718@hotmail.com>
2012-03-26 22:26:52 +0530

    used string value in config.max:

--- GTG/gtk/browser/browser.py
+++ GTG/gtk/browser/browser.py
@@ -445,11 +445,15 @@ class TaskBrowser(gobject.GObject):
     def quit(self,widget=None):
         self.vmanager.close_browser()
     
-	"""This event checks for the window state: maximized?
+          
+    def on_window_state_event(self,widget,event,data=None):
+
+         """This event checks for the window state: maximized?
 	    and stores the state in self.config.max
 	    This is used to check the window state afterwards
-	     and maximize it if needed"""    
-    def on_window_state_event(self,widget,event,data=None):
+	     and maximize it if needed
+	"""
+ 	
 	mask = gtk.gdk.WINDOW_STATE_MAXIMIZED
 	if widget.get_window().get_state() & mask == mask:	
 	   self.config.set("max","True")
