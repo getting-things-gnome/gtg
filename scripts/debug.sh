0666664ff2fd2d668803a8ece153c5fb890ab2f2
Izidor Matušov <izidor.matusov@gmail.com>
2012-06-07 16:00:34 +0200

    Added -t parameter to distinguish between multiple instances of GTG

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -10,6 +10,7 @@ args="--no-crash-handler"
 dataset="default"
 norun=0
 profile=0
+title=""
 
 # Create execution-time data directory if needed
 mkdir -p tmp
@@ -26,7 +27,8 @@ do  case "$o" in
     n)   norun=1;;
     p)   profile=1;;
     s)   dataset="$OPTARG";;
-    [?]) echo >&2 "Usage: $0 [-s dataset] [-b] [-d] [-l] [-n] [-p]"
+    t)   title="$OPTARG";;
+    [?]) echo >&2 "Usage: $0 [-s dataset] [-t title] [-b] [-d] [-l] [-n] [-p]"
          exit 1;;
     esac
 done
@@ -43,6 +45,17 @@ export XDG_DATA_HOME="./tmp/$dataset/xdg/data"
 export XDG_CACHE_HOME="./tmp/$dataset/xdg/cache"
 export XDG_CONFIG_HOME="./tmp/$dataset/xdg/config"
 
+# Title has to be passed to GTG directly, not through $args
+# title could be more word, and only the first word would be taken
+if [ "$title" = "" ]
+then
+    title="Dev GTG: $(basename `pwd`)"
+    if [ "$dataset" != "default" ]
+    then
+        title="$title ($dataset dataset)"
+    fi
+fi
+
 if [ $norun -eq 0 ]; then
     # Check for liblarch
     if ! ./GTG/tools/import_liblarch.py $liblarchArgs; then
@@ -57,10 +70,10 @@ if [ $norun -eq 0 ]; then
     fi
 
     if [ $profile -eq 1 ]; then
-	python -m cProfile -o gtg.prof ./gtg $args
-    python ./scripts/profile_interpret.sh
+        python -m cProfile -o gtg.prof ./gtg $args -t "$title"
+        python ./scripts/profile_interpret.sh
     else
-	./gtg $args
+	./gtg $args -t "$title"
     fi
 fi
 
