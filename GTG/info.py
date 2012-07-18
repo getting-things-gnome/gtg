c2969be8bff4ce0ec86da58e297f60d398c138ff
Bertrand Rousseau <bertrand.rousseau@gmail.com>
2012-07-18 14:07:18 +0200

    Update CHANGELOG, AUTHORS, info.py

--- GTG/info.py
+++ GTG/info.py
@@ -78,6 +78,7 @@ AUTHORS     = ["Main developers:", \
                "\tZach Shepherd <eightball1989@gmail.com>", \
                "\tZimin Huang <ienjoycoding@gmail.com>", \
                "\tNimit Shah <nimit.svnit@gmail.com>", \
+               "\tAntonio Roquentin <>", \
 ]
 ARTISTS     = ["Kalle Persson <kalle@kallepersson.se>", \
                 "Bertrand Rousseau <bertrand.rousseau@gmail.com>"]
