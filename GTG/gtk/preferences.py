13d7fdf56c998e72a44dd42e8415e838fb8c49ed
Abhiram <abhiram.ampabathina@gmail.com>
2012-08-02 20:35:01 +0530

    some changes to preferences.py

--- GTG/gtk/preferences.py
+++ GTG/gtk/preferences.py
@@ -18,7 +18,7 @@
 # -----------------------------------------------------------------------------
 
 """ The Preferences Dialog for configuring GTG """
-
+import pygtk
 import os
 import shutil
 
@@ -153,5 +153,5 @@ class PreferencesDialog:
             self.config.set("bg_color_enable", not curstate)
             self._refresh_task_browser()
     def on_font_change(self, widget):
-        self.config.set("font_name", get_font_name)
+        self.config.set("font_name", get_font_name())
         self._refresh_task_browser()
