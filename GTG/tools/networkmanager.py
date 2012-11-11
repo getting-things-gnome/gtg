f0c6df81ec0acc37fcf5f2cc7a69d99a9d0aa812
Izidor Matušov <izidor.matusov@gmail.com>
2012-11-11 19:59:12 +0100

    Use NetworkManger GIR instead of direct DBus communication

--- GTG/tools/networkmanager.py
+++ GTG/tools/networkmanager.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Gettings Things Gnome! - a personal organizer for the GNOME desktop
-# Copyright (c) 2008-2009 - Lionel Dricot & Bertrand Rousseau
+# Copyright (c) 2008-2012 - Lionel Dricot & Bertrand Rousseau
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -18,24 +18,14 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
 
-""" Communicate with Network Manager over its D-Bus API
+""" Communicate with Network Manager """
 
-API spec: http://projects.gnome.org/NetworkManager/developers/api/09/spec.html
-"""
-
-import dbus
-
-# A network device is connected, with global network connectivity. 
-NM_STATE_CONNECTED_GLOBAL = 70
+from gi.repository import NetworkManager, NMClient
 
 def is_connection_up():
     """ Returns True if GTG can access the Internet """
-    bus = dbus.SystemBus()
-    proxy = bus.get_object('org.freedesktop.NetworkManager', 
-                            '/org/freedesktop/NetworkManager')
-    network_manager = dbus.Interface(proxy, 'org.freedesktop.NetworkManager')
-
-    return network_manager.state() == NM_STATE_CONNECTED_GLOBAL
+    state = NMClient.Client().get_state()
+    return state == NetworkManager.State.CONNECTED_GLOBAL
 
 if __name__ == "__main__":
     print "is_connection_up() == %s" % is_connection_up()
