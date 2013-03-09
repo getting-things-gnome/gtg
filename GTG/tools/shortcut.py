7b7dc6262c3f845b98cf92cbfcf3c9267da363ff
Parin Porecha <parinporecha@gmail.com>
2013-03-09 19:54:31 +0530

    Fixed some bugs, and learnt some new things

--- GTG/tools/shortcut.py
+++ GTG/tools/shortcut.py
@@ -20,27 +20,28 @@
 import subprocess
 import re
 
-list_prefix = "gsettings list-keys "
-CHECK_VERSION = list_prefix + "org.gnome.settings-daemon.plugins.media-keys"
+
+CHECK_VERSION = "gsettings list-keys " \
+"org.gnome.settings-daemon.plugins.media-keys"
 NEW_TASK_ACTION = "gtg_new_task"
 NEW_TASK_NAME = "GTG New Task"
-get_temp = "gsettings get "
-path = "org.gnome.settings-daemon.plugins.media-keys "
-GSETTINGS_GET_LIST = get_temp + path + "custom-keybindings"
-set_temp = "gsettings set "
-GSETTINGS_SET_LIST = set_temp + path + "custom-keybindings"
-key_path1 = "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:"
-path2 = "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/"
-GSETTINGS_GET = get_temp + key_path1 + path2 + "custom"
-GSETTINGS_SET = set_temp + key_path1 + path2 + "custom"
+GSETTINGS_GET_LIST = "gsettings get " \
+"org.gnome.settings-daemon.plugins.media-keys custom-keybindings"
+GSETTINGS_SET_LIST = "gsettings set " \
+"org.gnome.settings-daemon.plugins.media-keys custom-keybindings"
+GSETTINGS_GET = "gsettings get " \
+"org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:" \
+"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom"
+GSETTINGS_SET = "gsettings set " \
+"org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:" \
+"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom"
 GCONF_GET = "gconftool-2 --get /desktop/gnome/keybindings/custom"
 GCONF_SET = "gconftool-2 --type string --set /desktop/gnome/keybindings/custom"
 
 
 def get_saved_binding():
     """ Get the current shortcut if the task exists """
-    list_keys = call_subprocess(cmd = CHECK_VERSION)
-    list_keys = list_keys.splitlines()
+    list_keys = call_subprocess(CHECK_VERSION).splitlines()
     if "custom-keybindings" in list_keys:
         binding = get_shortcut_from_dconf()
     else:
@@ -50,17 +51,14 @@ def get_saved_binding():
 
 def get_shortcut_from_dconf():
     """ If system uses dconf, then get the shortcut via gsettings """
-    dconf_out = call_subprocess(cmd = GSETTINGS_GET_LIST)
-    custom_shortcuts_list = re.findall(r'custom[0-9]+', dconf_out)
+    dconf_out = call_subprocess(GSETTINGS_GET_LIST)
+    custom_shortcuts_list = re.findall(r'\d+', dconf_out)
 
     for entry in custom_shortcuts_list:
-        to_access = entry[-1]
-        get_cmd = call_subprocess(cmd = GSETTINGS_GET, i = str(to_access),
-                                  key = "/ command")
+        get_cmd = call_subprocess(GSETTINGS_GET, entry, "/ command")
 
         if NEW_TASK_ACTION in get_cmd:
-            get_bind = call_subprocess(cmd = GSETTINGS_GET, i = str(to_access),
-                                       key = "/ binding")
+            get_bind = call_subprocess(GSETTINGS_GET, entry, "/ binding")
             return get_bind.strip("\'\n")
     return None
 
@@ -68,28 +66,24 @@ def get_shortcut_from_dconf():
 def get_shortcut_from_gconf():
     """ If system uses gconf, then get the shortcut via gconftool-2 """
     item=0
-    while(1):
-        get_action = call_subprocess(cmd = GCONF_GET, i = str(item),
-                                     key = "/action")
+    while True:
+        get_action = call_subprocess(GCONF_GET, str(item), "/action")
 
         if NEW_TASK_ACTION in get_action:
-            get_bind = call_subprocess(cmd = GCONF_GET, i = str(item),
-                                       key = "/binding")
+            get_bind = call_subprocess(GCONF_GET, str(item), "/binding")
             return get_bind.rstrip("\n")
 
         elif get_action == "":
-            get_name = call_subprocess(cmd = GCONF_GET, i = str(item),
-                                       key = "/name")
+            get_name = call_subprocess(GCONF_GET, str(item), "/name")
             if get_name == "":
                 return None
-    item += 1
+        item += 1
 
 
-def on_shortcut_change(binding, button_state):
+def save_new_binding(binding, button_state):
     """ When user has entered a new shortcut """
     if button_state == True:
-        list_keys = call_subprocess(cmd = CHECK_VERSION)
-        list_keys = list_keys.splitlines()
+        list_keys = call_subprocess(CHECK_VERSION).splitlines()
         if "custom-keybindings" in list_keys:
             add_shortcut_to_dconf(binding)
         else:
@@ -98,69 +92,53 @@ def on_shortcut_change(binding, button_state):
 
 def add_shortcut_to_dconf(binding):
     """ If system uses dconf, then set the new shortcut via gsettings """
-    dconf_out = call_subprocess(cmd = GSETTINGS_GET_LIST)
-    custom_keys = re.findall(r'custom[0-9]+', dconf_out)
+    dconf_out = call_subprocess(GSETTINGS_GET_LIST)
+    custom_keys = re.findall(r'\d+', dconf_out)
+    a = []
 
     for entry in custom_keys:
-        to_access = entry[-1]
-        get_cmd = call_subprocess(cmd = GSETTINGS_GET, i = str(to_access),
-                                  key = "/ command")
+        get_cmd = call_subprocess(GSETTINGS_GET, entry, "/ command")
 
         if NEW_TASK_ACTION in get_cmd:
-            call_subprocess(cmd = GSETTINGS_SET, to_append = binding,
-                            i = str(to_access), key = "/ binding")
+            call_subprocess(GSETTINGS_SET, entry, "/ binding", binding)
             return
+        a.append(int(entry))
 
-    a=[]
-    for item in custom_keys:
-        a.append(int(re.findall(r'[0-9]+', item)[0]))
     if a == []:
         index = 0
     else:
         a.sort()
         index = a[-1] + 1
-    prefix = "['/org/gnome/settings-daemon/plugins/media-keys/"
-    prefix = prefix + "custom-keybindings/custom"
-    append_this = prefix + str(index) + "/']"
-    call_subprocess(cmd = GSETTINGS_SET, to_append = NEW_TASK_ACTION,
-                    i = str(index), key = "/ command")
-    call_subprocess(cmd = GSETTINGS_SET, to_append = binding,
-                    i = str(index), key = "/ binding")
-    call_subprocess(cmd = GSETTINGS_SET, to_append = NEW_TASK_NAME,
-                    i = str(index), key = "/ name")
+    append_this = "['/org/gnome/settings-daemon/plugins/media-keys/" \
+    "custom-keybindings/custom" + str(index) + "/']"
+    call_subprocess(GSETTINGS_SET, str(index), "/ command", NEW_TASK_ACTION)
+    call_subprocess(GSETTINGS_SET, str(index), "/ binding", binding)
+    call_subprocess(GSETTINGS_SET, str(index), "/ name", NEW_TASK_NAME)
 
     if index == 0:
         call_subprocess(cmd = GSETTINGS_SET_LIST, key = ' ' + append_this)
 
     else:
-        result_list = dconf_out[:-2] + ", "
-        result_list = result_list + append_this[1:-1] + "]"
-        call_subprocess(cmd = GSETTINGS_SET_LIST,
-                        to_append = result_list)
+        result_list = dconf_out[:-2] + ", " + append_this[1:]
+        call_subprocess(cmd = GSETTINGS_SET_LIST, to_append = result_list)
 
 
 def add_shortcut_to_gconf(binding):
     """ If system uses gconf, then set the new shortcut via gconftool-2 """
     item=0
-    while(1):
-        get_action = call_subprocess(cmd = GCONF_GET, i = str(item),
-                                     key = "/action")
+    while True:
+        get_action = call_subprocess(GCONF_GET, str(item), "/action")
 
         if NEW_TASK_ACTION in get_action:
-            binding_ans = call_subprocess(cmd = GCONF_SET,
-                                          to_append = binding, i = str(item),
-                                          key = "/binding")
-            break
+            call_subprocess(GCONF_SET, str(item), "/binding", binding)
+            return
 
         if get_action == "":
-            call_subprocess(cmd = GCONF_SET, to_append = NEW_TASK_ACTION,
-                            i = str(item), key = "/action")
-            call_subprocess(cmd = GCONF_SET, to_append = binding,
-                            i = str(item), key = "/binding")
-            call_subprocess(cmd = GCONF_SET, to_append = NEW_TASK_NAME,
-                            i = str(item), key = "/name")
-            break
-    item += 1
+            call_subprocess(GCONF_SET, str(item), "/action", NEW_TASK_ACTION)
+            call_subprocess(GCONF_SET, str(item), "/binding", binding)
+            call_subprocess(GCONF_SET, str(item), "/name", NEW_TASK_NAME)
+            return
+        item += 1
 
 
 def call_subprocess(cmd, i = "", key = "", to_append = None):
@@ -169,12 +147,11 @@ def call_subprocess(cmd, i = "", key = "", to_append = None):
         'i' accesses a custom shortcut,
         'key' accesses values inside the shortcut,
         'to_append' contains the new value to replace if any """
-    cmd = cmd + i + key
+    cmd += i + key
     cmd = cmd.split(" ")
     if to_append != None:
         cmd.append(to_append)
-    out = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
+    out = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return out.communicate()[0]
 
 
