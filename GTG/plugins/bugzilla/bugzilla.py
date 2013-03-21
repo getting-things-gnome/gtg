090cb1399d43865e73fc87eaae5828298a7d6505
Chenxiong Qi <qcxhome@gmail.com>
2013-03-22 00:22:54 +0800

    Refactor code of Bugzilla plugin
    There are two main changes. One is plugin's code is rewritten, however it
    keeps original behavior to user. Another one is bugz is not a dependency,
    because Bugzilla plugin just gets a bug information via Bugzilla's XMLRPC
    Web service, and xmlrpclib is enough for this purpose. Furthermore, this
    solves bug #1075703 naturally.

--- GTG/plugins/bugzilla/bugzilla.py
+++ GTG/plugins/bugzilla/bugzilla.py
@@ -15,18 +15,77 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gobject
+import re
 import threading
 import xmlrpclib
 from urlparse import urlparse
 
-from GTG.plugins.bugzilla.server import ServersStore
-from GTG.plugins.bugzilla.bug import Bug
+from services import BugzillaServiceFactory
+
+__all__ = ('pluginBugzilla', )
+
+bugIdPattern = re.compile('^\d+$')
+
+
+class GetBugInformationTask(threading.Thread):
+
+    def __init__(self, task, **kwargs):
+        ''' Initialize task data, where task is the GTG task object. '''
+        self.task = task
+        super(GetBugInformationTask, self).__init__(**kwargs)
+
+    def parseBugUrl(self, url):
+        r = urlparse(url)
+        queries = dict([item.split('=') for item in r.query.split('&')])
+        return r.scheme, r.hostname, queries
+
+    def run(self):
+        bug_url = self.task.get_title()
+        scheme, hostname, queries = self.parseBugUrl(bug_url)
+
+        bug_id = queries.get('id', None)
+        if bugIdPattern.match(bug_id) is None:
+            # FIXME: make some sensable action instead of returning silently.
+            return
+
+        try:
+            bugzillaService = BugzillaServiceFactory.create(scheme, hostname)
+        except Exception, err:
+            # When cannot get a Bugzilla service whatever due to any reason,
+            # do nothing and return just.
+            print err
+            return
+
+        try:
+            bug = bugzillaService.getBug(bug_id)
+        except xmlrpclib.Fault, err:
+            code = err.faultCode
+            if code == 100:  # invalid bug ID
+                title = 'Invalid bug ID #%s' % bug_id
+            elif code == 101:  # bug ID not exist
+                title = 'Bug #%s does not exist.' % bug_id
+            elif code == 102:  # Access denied
+                title = 'Access denied to bug #%s' % bug_id
+            else:  # unrecoganized error code currently
+                title = err.faultString
+
+            old_title = self.task.title
+            gobject.idle_add(self.task.set_title, title)
+            gobject.idle_add(self.task.set_text, old_title)
+        except Exception, err:
+            print err
+        else:
+            title = '#%s: %s' % (bug_id, bug.summary)
+            gobject.idle_add(self.task.set_title, title)
+            text = "%s\n\n%s" % (bug_url, bug.description)
+            gobject.idle_add(self.task.set_text, text)
+
+            tag = bugzillaService.getTag(bug)
+            if tag is not None:
+                gobject.idle_add(self.task.add_tag, '@%s' % tag)
 
 
 class pluginBugzilla:
-
-    def __init__(self):
-        self.servers = ServersStore()
 
     def activate(self, plugin_api):
         self.plugin_api = plugin_api
@@ -37,62 +96,13 @@ class pluginBugzilla:
         # this is a gobject callback that will block the Browser.
         # decoupling with a thread. All interaction with task and tags objects
         #(anything in a Tree) must be done with gobject.idle_add (invernizzi)
-        thread = threading.Thread(target=self.__analyze_task,
-                                  args=(task_id, ))
-        thread.setDaemon(True)
-        thread.start()
 
-    def __analyze_task(self, task_id):
         task = self.plugin_api.get_requester().get_task(task_id)
-        url = task.get_title()
-        r = urlparse(url)
-        if r.hostname is None:
-            return
-
-        server = self.servers.get(r.hostname)
-        if server is None:
-            return
-
-        base = '%s://%s/xmlrpc.cgi' % (r.scheme, server.name)
-
-        # get the number of the bug
-        try:
-            nb = r.query.split('id=')[1]
-        except IndexError:
-            return
-
-        try:
-            bug = Bug(base, nb)
-        except xmlrpclib.Fault, err:
-            code = err.faultCode
-            if code == 100:  # invalid bug ID
-                title = 'Invalid bug ID #%s' % nb
-            elif code == 101:  # bug ID not exist
-                title = 'Bug #%s does not exist.' % nb
-            elif code == 102:  # Access denied
-                title = 'Access denied to bug #%s' % nb
-            else:  # unrecoganized error code currently
-                title = err.faultString
-            old_title = task.get_title()
-            gobject.idle_add(task.set_title, title)
-            gobject.idle_add(task.set_text, old_title)
-            return
-        except:
-            return
-
-        title = bug.get_title()
-        if title is None:
-            # can't find the title of the bug
-            return
-
-        gobject.idle_add(task.set_title, '#%s: %s' % (nb, title))
-
-        text = "%s\n\n%s" % (url, bug.get_description())
-        gobject.idle_add(task.set_text, text)
-
-        tag = server.get_tag(bug)
-        if tag is not None:
-            gobject.idle_add(task.add_tag, '@%s' % tag)
+        bugTask = GetBugInformationTask(task)
+        bugTask.run()
+        return
+        bugTask.setDaemon(True)
+        bugTask.start()
 
     def deactivate(self, plugin_api):
         plugin_api.get_ui().disconnect(self.connect_id)
