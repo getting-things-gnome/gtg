e3a4fd233a5d3bc533d2bcd490a9897d0a8f6613
Parin Porecha <parinporecha@gmail.com>
2013-11-23 20:10:23 +0530

    Changed all instances of 2008-2012 to 2008-2013 and updated version to 0.3.1

--- GTG/tools/import_liblarch.py
+++ GTG/tools/import_liblarch.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # -----------------------------------------------------------------------------
 # Getting Things GNOME! - A personal organizer for the GNOME desktop
-# Copyright (c) 2008-2012 Lionel Dricot & Bertrand Rousseau
+# Copyright (c) 2008-2013 Lionel Dricot & Bertrand Rousseau
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
