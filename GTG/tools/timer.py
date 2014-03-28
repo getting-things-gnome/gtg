70945e95bb9d8fdaecfa1e4aae19ebf11e21c5e3
dawdler <atit.anand.cs@gmail.com>
2014-03-29 02:03:42 +0530

    CHANGELOG modified

--- GTG/tools/timer.py
+++ GTG/tools/timer.py
@@ -17,7 +17,7 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
 
-""" General class for representing time and periodic time intervals in GTG"""
+""" General class for representing time and periodic time intervals in GTG """
 
 import datetime
 
