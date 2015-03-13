f3cd1e754c643043faab911656f94409ddd94845
Kunaal Jain <kunaalus@gmail.com>
2015-03-13 03:12:16 -0700

    Send Task via Email Plugin
    Added a button to the main window toolbar
    so that users can send task as email from main window as requested in
    bug #1252139
    Few minor bug fixes and improvements

--- GTG/plugins/send_email/sendEmail.py
+++ GTG/plugins/send_email/sendEmail.py
@@ -37,34 +37,32 @@ class pluginSendEmail:
         """
         Initialize variables.
         """
-        self.plugin_api = None
+        self.browser_plugin_api = None
+        self.taskview_plugin_api = None
         self.tb_Managerbutton = None
         self.tb_Taskbutton = None
-        self.button = None
 
     def activate(self, plugin_api):
         """
-        Calls the gtk function to show button on manager.
+        Adds the task button on manager.
         """
-        self.plugin_api = plugin_api
-        self.req = self.plugin_api.get_requester()
-        self.newButton(plugin_api)
-        self.tb_Managerbutton = self.button
+        self.browser_plugin_api = plugin_api
+        self.req = plugin_api.get_requester()
+        self.tb_Managerbutton = self.get_new_button()
         self.tb_Managerbutton.connect('clicked', self.onTbManagerButton, plugin_api)
         self.tb_Managerbutton.show()
-        self.plugin_api.add_toolbar_item(self.tb_Managerbutton)
-        self.plugin_api.set_active_selection_changed_callback(
+        plugin_api.add_toolbar_item(self.tb_Managerbutton)
+        plugin_api.set_active_selection_changed_callback(
             self.selection_changed)
 
     def onTaskOpened(self, plugin_api):
         """
         Adds the button when a task is opened.
         """
-        self.plugin_api = plugin_api
         # add a item (button) to the ToolBar
-        self.newButton(plugin_api)
-        self.button.set_sensitive(True)
-        self.tb_Taskbutton = self.button
+        self.taskview_plugin_api = plugin_api
+        self.tb_Taskbutton = self.get_new_button()
+        self.tb_Taskbutton.set_sensitive(True)
         self.tb_Taskbutton.connect('clicked', self.onTbTaskButton, plugin_api)
         self.tb_Taskbutton.show()
         plugin_api.add_toolbar_item(self.tb_Taskbutton)
@@ -76,14 +74,14 @@ class pluginSendEmail:
         # everything should be removed, in case a task is currently opened
 
         if self.tb_Taskbutton:
-            self.plugin_api.remove_toolbar_item(self.tb_Taskbutton)
-            self.tb_Taskbutton = False
-            self.plugin_api.remove_toolbar_item(self.tb_Managerbutton)
-            self.tb_Managerbutton = False
+            self.taskview_plugin_api.remove_toolbar_item(self.tb_Taskbutton)
+            self.tb_Taskbutton = None
+            self.browser_plugin_api.remove_toolbar_item(self.tb_Managerbutton)
+            self.tb_Managerbutton = None
 
         if self.tb_Managerbutton:
-            self.plugin_api.remove_toolbar_item(self.tb_Managerbutton)
-            self.tb_Managerbutton = False
+            self.browser_plugin_api.remove_toolbar_item(self.tb_Managerbutton)
+            self.tb_Managerbutton = None
 
 
 
@@ -97,14 +95,14 @@ class pluginSendEmail:
         else:
             self.tb_Managerbutton.set_sensitive(False)
 
-    def newButton(self, plugin_api):
-        """ Initialize all the GTK widgets """
-        self.button = True
-        self.button = Gtk.ToolButton()
-        self.button.set_sensitive(False)
-        self.button.set_icon_name('mail-send')
-        self.button.set_is_important(True)
-        self.button.set_label(_("Send via email"))
+    def get_new_button(self):
+        """ Initialize a new button """
+        button = Gtk.ToolButton()
+        button.set_sensitive(False)
+        button.set_icon_name('mail-send')
+        button.set_is_important(True)
+        button.set_label(_("Send via email"))
+        return button
 
     def onTbTaskButton(self, widget, plugin_api):
         """
@@ -117,14 +115,14 @@ class pluginSendEmail:
         """
         When the user presses the button in manager.
         """
-        for tid in self.plugin_api.get_selected():
+        for tid in plugin_api.get_selected():
             task = self.req.get_task(tid)
             self.sendEmail(task)
 
 
     def sendEmail(self, task):
         """
-        # Body contains Status Tags, Subtasks and Content.
+        Body contains Status Tags, Subtasks and Content.
         """
         body = _("Status: %s") % (task.get_status()) + \
             _("\nTags: %s") % (", ".join(task.get_tags_name())) + \
