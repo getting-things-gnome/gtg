d6d8e7e6db944652f87d11d9a1fe8fd01fd5ef0f
jdiez17 <jose.manuel.diez@gmail.com>
2015-06-28 14:16:58 +0200

    gtk/browser: Ensure both times are instances of datetime, required for accurate time comparison.

--- GTG/gtk/browser/treeview_factory.py
+++ GTG/gtk/browser/treeview_factory.py
@@ -32,6 +32,8 @@ from GTG.tools.dates import Date
 from liblarch_gtk import TreeView
 
 
+from datetime import datetime
+
 class TreeviewFactory():
 
     def __init__(self, requester, config):
@@ -183,6 +185,14 @@ class TreeviewFactory():
             elif para == 'added':
                 t1 = task1.get_added_date()
                 t2 = task2.get_added_date()
+
+                # Convert both times to datetimes (accurate comparison)
+                if isinstance(t1, Date):
+                    d = t1.date()
+                    t1 = datetime(year=d.year, month=d.month, day=d.day)
+                if isinstance(t2, Date):
+                    d = t2.date()
+                    t2 = datetime(year=d.year, month=d.month, day=d.day)
             else:
                 raise ValueError(
                     'invalid date comparison parameter: %s') % para
