6b8f94ea20cfe7d027f84abcb22210656fe0e4db
Diego Garcia Gangl <dnicolas@gmail.com>
2019-12-06 20:08:18 -0300

    Clear search field when hiding

--- GTG/gtk/browser/browser.py
+++ GTG/gtk/browser/browser.py
@@ -421,6 +421,7 @@ class TaskBrowser(GObject.GObject):
         if self.searchbar.get_search_mode():
             self.search_button.set_active(False)
             self.searchbar.set_search_mode(False)
+            self.search_entry.set_text('')
         else:
             self.search_button.set_active(True)
             self.searchbar.set_search_mode(True)
