3ca59b19964fb3153f571372259eba996c91052f
Diego Garcia Gangl <dnicolas@gmail.com>
2019-12-20 20:54:21 -0300

    Use Gtk.MessageDialog for the task delete dialog

--- GTG/gtk/delete_dialog.py
+++ GTG/gtk/delete_dialog.py
@@ -23,111 +23,116 @@ from gi.repository import Gtk
 from GTG.core.translations import _, ngettext
 from GTG.gtk import ViewConfig
 
-
 class DeletionUI(object):
 
     MAXIMUM_TIDS_TO_SHOW = 5
 
-    def __init__(self, req):
+    def __init__(self, req, window):
         self.req = req
         self.tids_todelete = []
+
         # Tags which must be updated
         self.update_tags = []
-        # Load window tree
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file(ViewConfig.DELETE_UI_FILE)
-        signals = {"on_delete_confirm": self.on_delete_confirm,
-                   "on_delete_cancel": lambda x: x.hide, }
-        self.builder.connect_signals(signals)
+        self.window = window
 
-    def on_delete_confirm(self, widget):
+    def on_delete_confirm(self):
         """if we pass a tid as a parameter, we delete directly
         otherwise, we will look which tid is selected"""
+
         for tid in self.tids_todelete:
             if self.req.has_task(tid):
                 self.req.delete_task(tid, recursive=True)
+
         self.tids_todelete = []
 
         # Update tags
         for tagname in self.update_tags:
             tag = self.req.get_tag(tagname)
             tag.modified()
+
         self.update_tags = []
 
-    def delete_tasks(self, tids=None):
-        if tids:
-            self.tids_todelete = tids
-        # We must at least have something to delete !
-        if len(self.tids_todelete) > 0:
-            tasklist = []
-            self.update_tags = []
-            for tid in self.tids_todelete:
+    def recursive_list_tasks(self, tasklist, root):
+        """Populate a list of all the subtasks and
+           their children, recursively.
 
-                def recursive_list_tasks(task_list, root):
-                    """Populate a list of all the subtasks and
-                       their children, recursively.
+           Also collect the list of affected tags
+           which should be refreshed"""
 
-                       Also collect the list of affected tags
-                       which should be refreshed"""
-                    if root not in task_list:
-                        task_list.append(root)
-                        for tagname in root.get_tags_name():
-                            if tagname not in self.update_tags:
-                                self.update_tags.append(tagname)
-                        for i in root.get_subtasks():
-                            if i not in task_list:
-                                recursive_list_tasks(task_list, i)
+        if root not in tasklist:
+            tasklist.append(root)
 
-                task = self.req.get_task(tid)
-                recursive_list_tasks(tasklist, task)
+            [self.update_tags.append(tagname)
+             for tagname in root.get_tags_name()
+             if tagname not in self.update_tags]
 
-            # We fill the text and the buttons' labels according to the number
-            # of tasks to delete
-            label = self.builder.get_object("label1")
-            label_text = label.get_text()
-            cdlabel2 = self.builder.get_object("cd_question_label")
-            cdlabel3 = self.builder.get_object("cd_cancel_label")
-            cdlabel4 = self.builder.get_object("cd_delete_label")
-            singular = len(tasklist)
-            label_text = ngettext("Deleting a task cannot be undone, "
-                                  "and will delete the following task: ",
-                                  "Deleting a task cannot be undone, "
-                                  "and will delete the following tasks: ",
-                                  singular)
-            cdlabel2.set_label(ngettext("Are you sure you want to delete this"
-                                        " task?",
-                                        "Are you sure you want to delete "
-                                        "these tasks?",
-                                        singular))
+            [self.recursive_list_tasks(tasklist, i)
+             for i in root.get_subtasks() if i not in tasklist]
 
-            cdlabel3.set_label(ngettext("Keep selected task",
-                                        "Keep selected tasks",
-                                        singular))
-            cdlabel4.set_label(ngettext("Permanently remove task",
-                                        "Permanently remove tasks",
-                                        singular))
-            label_text = label_text[0:label_text.find(":") + 1]
 
-            # we don't want to end with just one task that doesn't fit the
-            # screen and a line saying "And one more task", so we go a
-            # little over our limit
-            missing_titles_count = len(tasklist) - self.MAXIMUM_TIDS_TO_SHOW
-            if missing_titles_count >= 2:
-                tasks = tasklist[: self.MAXIMUM_TIDS_TO_SHOW]
-                titles_suffix = _("\nAnd %d more tasks" % missing_titles_count)
-            else:
-                tasks = tasklist
-                titles_suffix = ""
+    def show(self, tids=None):
+        self.tids_todelete = tids or self.tids_todelete
 
-            titles = "".join("\n - " + task.get_title() for task in tasks)
-            label.set_text(label_text + titles + titles_suffix)
-            delete_dialog = self.builder.get_object("confirm_delete")
-            delete_dialog.resize(1, 1)
-            cancel_button = self.builder.get_object("cancel")
-            cancel_button.grab_focus()
-            if delete_dialog.run() != 1:
-                tasklist = []
-            delete_dialog.hide()
-            return tasklist
-        else:
+        if not self.tids_todelete:
+            # We must at least have something to delete!
             return []
+
+        # Get full task list to delete
+        tasklist = []
+        self.update_tags = []
+
+        for tid in self.tids_todelete:
+            task = self.req.get_task(tid)
+            self.recursive_list_tasks(tasklist, task)
+
+        # Prepare Labels
+        singular = len(tasklist)
+        cancel_text = ngettext("Keep selected task",
+                               "Keep selected tasks",
+                                singular)
+
+        delete_text = ngettext("Permanently remove task",
+                               "Permanently remove tasks",
+                                singular)
+
+        label_text = ngettext("Deleting a task cannot be undone, "
+                              "and will delete the following task: ",
+                              "Deleting a task cannot be undone, "
+                              "and will delete the following tasks: ",
+                              singular)
+
+        label_text = label_text[0:label_text.find(":") + 1]
+
+        missing_titles_count = len(tasklist) - self.MAXIMUM_TIDS_TO_SHOW
+
+        if missing_titles_count >= 2:
+            tasks = tasklist[: self.MAXIMUM_TIDS_TO_SHOW]
+            titles_suffix = _("\nAnd %d more tasks" % missing_titles_count)
+        else:
+            tasks = tasklist
+            titles_suffix = ""
+
+        titles = "".join("\n• " + task.get_title() for task in tasks)
+
+
+        # Build and run dialog
+        dialog = Gtk.MessageDialog(transient_for=self.window, modal=True)
+        dialog.add_button(cancel_text, Gtk.ResponseType.CANCEL)
+
+        delete_btn = dialog.add_button(delete_text, Gtk.ResponseType.YES)
+        delete_btn.get_style_context().add_class("destructive-action")
+
+        dialog.props.use_markup = True
+        dialog.props.text = "<span weight=\"bold\">" + label_text + "</span>"
+
+        dialog.props.secondary_text = titles + titles_suffix
+
+        response = dialog.run()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.YES:
+            self.on_delete_confirm()
+        elif response == Gtk.ResponseType.REJECT:
+            tasklist = []
+
+        return tasklist
