791be96fd8a7a742fdb183494600242aeb507612
Diego Garcia Gangl <dnicolas@gmail.com>
2020-02-01 21:52:25 -0300

    Fix path to css file

--- GTG/core/dirs.py
+++ GTG/core/dirs.py
@@ -39,6 +39,7 @@ local_rootdir = os.path.abspath(
 
 # Icons from local folder
 ICONS_DIR = os.path.join(local_rootdir, 'data', 'icons')
+CSS_DIR = os.path.join(local_rootdir, 'data')
 
 # Where data & cache for synchronization services is stored
 SYNC_DATA_DIR = os.path.join(DATA_DIR, 'backends')
