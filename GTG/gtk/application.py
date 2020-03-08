dc6ad663b6017d477feb3dc0b4e90f8ac5e2ecca
Diego Garcia Gangl <dnicolas@gmail.com>
2020-03-08 19:04:31 -0300

    Insert app actions into dialogs
    This is necessary so those dialogs can access the actions, otherwise
    buttons/menu items will be disabled.

--- GTG/gtk/application.py
+++ GTG/gtk/application.py
@@ -99,8 +99,9 @@ class Application(Gtk.Application):
         self.edit_backends_dialog = None
 
         # Preferences and Backends windows
-        # Initialize  dialogs
         self.preferences = Preferences(self.req, self)
+
+        # Initialize  dialogs
         self.plugins = PluginsDialog(self.req)
 
         # Load custom css
@@ -163,6 +164,8 @@ class Application(Gtk.Application):
 
             if accel is not None:
                 self.set_accels_for_action(*accel)
+
+        self.plugins.dialog.insert_action_group('app', self)
 
 
     # Browser ##############################################################
@@ -313,6 +316,8 @@ class Application(Gtk.Application):
     def open_edit_backends(self, sender=None, backend_id=None):
         if not self.edit_backends_dialog:
             self.edit_backends_dialog = BackendsDialog(self.req)
+            self.edit_backends_dialog.dialog.insert_action_group('app', self)
+
         self.edit_backends_dialog.activate()
         if backend_id is not None:
             self.edit_backends_dialog.show_config_for_backend(backend_id)
