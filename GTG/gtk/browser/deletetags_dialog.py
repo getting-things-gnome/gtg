6b51ebf018daabdd0f3f99d7b9c517d3e3a342c2
Kris Thomsen <mail@kristhomsen.dk>
2020-03-15 21:38:17 +0100

    Adapt delete tag dialog to modern UI

--- GTG/gtk/browser/deletetags_dialog.py
+++ GTG/gtk/browser/deletetags_dialog.py
@@ -20,8 +20,6 @@
 from gi.repository import Gtk
 
 from GTG.core.translations import _, ngettext
-from GTG.gtk.browser import GnomeConfig
-
 
 class DeleteTagsDialog():
 
@@ -31,67 +29,73 @@ class DeleteTagsDialog():
         self.req = req
         self.browser = browser
         self.tags_todelete = []
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file(GnomeConfig.DELETETAGS_UI_FILE)
-        signals = {
-            "on_delete_confirm": self.browser.on_delete_tag,
-            "on_delete_cancel": lambda x: x.hide, }
-        self.builder.connect_signals(signals)
+
+    def on_delete_confirm(self):
+        """if we pass a tid as a parameter, we delete directly
+        otherwise, we will look which tid is selected"""
+
+        for tag in self.tags_todelete:
+            self.req.delete_tag(tag)
+
+        self.tags_todelete = []
 
     def delete_tags(self, tags=None):
-        if tags:
-            self.tags_todelete = tags
-        # We must at least have something to delete !
-        if len(self.tags_todelete) > 0:
+        self.tags_todelete = tags or self.tags_todelete
 
-            # We fill the text and the buttons' labels according to the number
-            # of tags to delete
-            label = self.builder.get_object("label1")
-            label_text = label.get_text()
-            cdlabel2 = self.builder.get_object("cd-label2")
-            cdlabel3 = self.builder.get_object("cd-label3")
-            cdlabel4 = self.builder.get_object("cd-label4")
-            singular = len(self.tags_todelete)
-            label_text = ngettext("Deleting a tag cannot be undone, "
-                                  "and will delete the following tag: ",
-                                  "Deleting a tag cannot be undone, "
-                                  "and will delete the following tag: ",
-                                  singular)
-            cdlabel2.set_label(ngettext("Are you sure you want to delete this"
-                                        " tag?",
-                                        "Are you sure you want to delete "
-                                        "these tags?",
-                                        singular))
-
-            cdlabel3.set_label(ngettext("Keep selected tag",
-                                        "Keep selected tags",
-                                        singular))
-            cdlabel4.set_label(ngettext("Permanently remove tag",
-                                        "Permanently remove tags",
-                                        singular))
-            label_text = label_text[0:label_text.find(":") + 1]
-
-            # we don't want to end with just one task that doesn't fit the
-            # screen and a line saying "And one more task", so we go a
-            # little over our limit
-            tags_count = len(self.tags_todelete)
-            missing_tags_count = tags_count - self.MAXIMUM_TAGS_TO_SHOW
-            if missing_tags_count >= 2:
-                tagslist = self.tags_todelete[:self.MAXIMUM_TAGS_TO_SHOW]
-                titles_suffix = _("\nAnd %d more tags") % missing_tags_count
-            else:
-                tagslist = self.tags_todelete
-                titles_suffix = ""
-
-            titles = "".join("\n - " + tag for tag in tagslist)
-            label.set_text(label_text + titles + titles_suffix)
-            delete_dialog = self.builder.get_object("confirm_delete_tag")
-            delete_dialog.resize(1, 1)
-            cancel_button = self.builder.get_object("cancel")
-            cancel_button.grab_focus()
-            if delete_dialog.run() != 1:
-                self.tags_todelete = []
-            delete_dialog.hide()
-            return tagslist
-        else:
+        if not self.tags_todelete:
+            # We must at least have something to delete !
             return []
+
+        # Prepare labels
+        singular = len(self.tags_todelete)
+        cancel_text = ngettext("Keep selected tag",
+                               "Keep selected tags",
+                                singular)
+
+        delete_text = ngettext("Permanently remove tag",
+                               "Permanently remove tags",
+                                singular)
+
+        label_text = ngettext("Deleting a tag cannot be undone, "
+                              "and will delete the following tag: ",
+                              "Deleting a tag cannot be undone, "
+                              "and will delete the following tag: ",
+                              singular)
+
+        label_text = label_text[0:label_text.find(":") + 1]
+
+        # we don't want to end with just one task that doesn't fit the
+        # screen and a line saying "And one more task", so we go a
+        # little over our limit
+        tags_count = len(self.tags_todelete)
+        missing_tags_count = tags_count - self.MAXIMUM_TAGS_TO_SHOW
+        if missing_tags_count >= 2:
+            tagslist = self.tags_todelete[:self.MAXIMUM_TAGS_TO_SHOW]
+            titles_suffix = _("\nAnd %d more tags") % missing_tags_count
+        else:
+            tagslist = self.tags_todelete
+            titles_suffix = ""
+
+        titles = "".join("\n• " + tag for tag in tagslist)
+
+        # Build and run dialog
+        dialog = Gtk.MessageDialog(transient_for=self.browser, modal=True)
+        dialog.add_button(cancel_text, Gtk.ResponseType.CANCEL)
+
+        delete_btn = dialog.add_button(delete_text, Gtk.ResponseType.YES)
+        delete_btn.get_style_context().add_class("destructive-action")
+
+        dialog.props.use_markup = True
+        dialog.props.text = "<span weight=\"bold\">" + label_text + "</span>"
+
+        dialog.props.secondary_text = titles + titles_suffix
+
+        response = dialog.run()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.YES:
+            self.on_delete_confirm()
+        elif response == Gtk.ResponseType.REJECT:
+            tagslist = []
+
+        return tagslist
