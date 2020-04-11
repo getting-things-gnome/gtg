2fea8ab9b3a9727e9eb6c370721d0ced2dc1f77c
Diego Garcia Gangl <dnicolas@gmail.com>
2020-04-11 19:41:17 -0300

    Fix signals on backends dialog
    fixes #284

--- GTG/gtk/application.py
+++ GTG/gtk/application.py
@@ -333,12 +333,12 @@ class Application(Gtk.Application):
         self.browser.about.show()
 
     def open_edit_backends(self, sender=None, backend_id=None):
-        if not self.edit_backends_dialog:
-            self.edit_backends_dialog = BackendsDialog(self.req)
-            self.edit_backends_dialog.dialog.insert_action_group('app', self)
+        self.edit_backends_dialog = BackendsDialog(self.req)
+        self.edit_backends_dialog.dialog.insert_action_group('app', self)
 
         self.edit_backends_dialog.activate()
-        if backend_id is not None:
+
+        if backend_id:
             self.edit_backends_dialog.show_config_for_backend(backend_id)
 
     def configure_backend(self, backend_id):
