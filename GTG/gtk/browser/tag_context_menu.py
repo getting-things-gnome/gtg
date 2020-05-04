d80ed63b58297f2fbb10d3ccd83d5aac86e7be07
Josh Crompton <josh.crompton@seequent.com>
2020-05-04 20:38:23 +1200

    Hide delete tag option in context menu for saved search
    Deleting the tag in this case doesn't do anything since it's actually
    a saved search, without a '@' prepended to the text.
    
    Fixes #283

--- GTG/gtk/browser/tag_context_menu.py
+++ GTG/gtk/browser/tag_context_menu.py
@@ -53,22 +53,26 @@ class TagContextMenu(Gtk.Menu):
             # Color chooser FIXME: SHOULD BECOME A COLOR PICKER
             self.mi_cc = Gtk.MenuItem()
             self.mi_cc.set_label(_("Edit Tag..."))
+            self.append(self.mi_cc)
+            self.mi_cc.connect('activate', self.on_mi_cc_activate)
+
             self.mi_ctag = Gtk.MenuItem()
             self.mi_ctag.set_label(_("Generate Color"))
-            self.mi_del_tag = Gtk.MenuItem()
-            self.mi_del_tag.set_label(_("Delete Tag"))
-            self.append(self.mi_cc)
             self.append(self.mi_ctag)
-            self.append(self.mi_del_tag)
-            self.mi_del_tag.connect(
-                'activate', self.app.browser.on_delete_tag_activate)
-            self.mi_cc.connect('activate', self.on_mi_cc_activate)
             self.mi_ctag.connect('activate', self.on_mi_ctag_activate)
+
             if self.tag.is_search_tag():
                 self.mi_del = Gtk.MenuItem()
                 self.mi_del.set_label(_("Delete"))
                 self.append(self.mi_del)
                 self.mi_del.connect('activate', self.on_mi_del_activate)
+            else:
+                self.mi_del_tag = Gtk.MenuItem()
+                self.mi_del_tag.set_label(_("Delete Tag"))
+                self.append(self.mi_del_tag)
+                self.mi_del_tag.connect(
+                    'activate', self.app.browser.on_delete_tag_activate)
+
         # Make it visible
         self.show_all()
 
