aa81bbf3b90f9262aca99259307266b99ab55372
Mart Raudsepp <leio@gentoo.org>
2020-05-22 20:59:19 +0300

    Drop obsolete translation related scripts
    meson generated ninja targets can be used for these instead, which will
    honor the list of files to be translated, will support desktop and appdata
    translation, etc:
    `ninja gtg-pot` does what generate_potfile.sh did
    `ninja gtg-update-po` does what generate_mofile.sh did

--- scripts/generate_potfile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-POTFILE="po/gtg.pot"
-rm ${POTFILE}
-touch ${POTFILE}
-find GTG/ -iname "*.ui" -exec intltool-extract --type=gettext/glade {} \;
-find GTG/ \( -iname "*.py" -o -iname "*.ui.h" \) -exec xgettext -j --language=Python --keyword=_ --keyword=N_ --from-code utf-8 --output=po/gtg.pot {} \;
