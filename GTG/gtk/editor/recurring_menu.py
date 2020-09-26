fa7c10f217787db1d57304afa9a257c5eaa2b049
zeddo123 <m.drissi@protonmail.com>
2020-09-26 14:17:55 +0200

    Changes related to indicating recurrency
    - Setting the Indicator moves to the taskview
    	The unicode indicator was being appended to the title itself.
    	It is now added when displaying the title in the taskview.
    
    change the color of the repeating icon to blue when the task is repeating

--- GTG/gtk/editor/recurring_menu.py
+++ GTG/gtk/editor/recurring_menu.py
@@ -1,3 +1,4 @@
+from gi.repository import Gdk, Gtk, Pango
 class RecurringMenu():
     """ RecurringMenu provides a simple layer of abstraction
     for the menu where the user enables a task to be repeating
@@ -11,10 +12,14 @@ class RecurringMenu():
         # Getting the necessary Gtk objects
         self.title = builder.get_object("title_label")
         self.repeat_button = builder.get_object("repeat_checkbutton")
+        self.repeat_icon = builder.get_object("repeat_icon")
+        self.icon_style = self.repeat_icon.get_style_context()
 
         # Update the editor using the task recurring status
         self.update_header()
         self.repeat_button.set_active(task.get_recurring())
+        if task.get_recurring():
+            self.icon_style.add_class('recurring-active')
 
     def is_term_set(self):
         return self.selected_recurring_term is not None
@@ -25,9 +30,13 @@ class RecurringMenu():
     def update_task(self):
         if self.repeat_button.get_active() and self.is_term_set():
             self.task.set_recurring(True, self.selected_recurring_term, newtask=True)
+            self.icon_style.remove_class('recurring-inactive')
+            self.icon_style.add_class('recurring-active')
             return True
         else:
             self.task.set_recurring(False)
+            self.icon_style.remove_class('recurring-active')
+            self.icon_style.add_class('recurring-inactive')
             return True
         return False
 
