70fec7fa600fa8304dfee8e55d96796f9ef8d098
Diego Garcia Gangl <dnicolas@gmail.com>
2020-11-02 21:14:51 -0300

    Move quick add parsing to its own module
    Add unit tests too

---
+++ GTG/gtk/browser/quick_add.py
@@ -0,0 +1,107 @@
+# -----------------------------------------------------------------------------
+# Getting Things GNOME! - a personal organizer for the GNOME desktop
+# Copyright (c)  - The GTG Team
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+"""Functionality for the Quick Add entry."""
+
+import re
+from typing import Dict
+
+from gettext import gettext as _
+
+from GTG.core.dates import Date
+from GTG.gtk.editor.taskview import TAG_REGEX
+
+
+TAGS_TOKEN = (
+    "tags",
+    "tag",
+    # Translators: Used in parsing, no spaces, lowercased in code
+    _("tags").lower(),
+    # Translators: Used in parsing, no spaces, lowercased in code
+    _("tag").lower(),
+)
+
+START_TOKEN = (
+    "defer",
+    "start",
+    "starts",
+    # Translators: Used in parsing, no spaces, lowercased in code
+    _("defer").lower(),
+    # Translators: Used in parsing, no spaces, lowercased in code
+    _("start").lower(),
+)
+
+DUE_TOKEN = ("due", _("due").lower())
+REPEAT_TOKEN = ("every", _("every").lower())
+
+# Math a word ending in :, followed by a space and some text.
+# Examples: "start: monday", "due: friday", "tags: home,pet"
+TOKEN_REGEX = re.compile('([\s]*)([\w-]+):\s*([^\s]+)')
+
+
+def parse(text: str) -> Dict:
+    """Parse contents of the quick add input."""
+
+    result = {
+        'title': '',
+        'tags': set(),
+        'start': None,
+        'due': None,
+        'recurring': None
+    }
+
+    for match in re.finditer(TAG_REGEX, text):
+        data = match.group(0)
+        result['tags'].add(data[1:])
+
+    for match in re.finditer(TOKEN_REGEX, text):
+        token = match.group(2)
+        data = match.group(3)
+
+        if token in TAGS_TOKEN:
+            for t in data.split(','):
+                # Strip @
+                if t.startswith('@'):
+                    t = t[1:]
+
+                result['tags'].add(t)
+
+        elif token in START_TOKEN:
+            try:
+                result['start'] = Date.parse(data)
+            except ValueError:
+                continue
+
+        elif token in DUE_TOKEN:
+            try:
+                result['due'] = Date.parse(data)
+            except ValueError:
+                continue
+
+        elif token in REPEAT_TOKEN:
+            try:
+                Date.today().parse_from_date(data)
+                result['recurring'] = data
+            except ValueError:
+                continue
+
+        # Remove this part from the title
+        text = text[:match.start()] + text[match.end():]
+
+    result['title'] = text
+    return result
---
+++ tests/core/test_quickadd.py
@@ -0,0 +1,234 @@
+# -----------------------------------------------------------------------------
+# Getting Things GNOME! - a personal organizer for the GNOME desktop
+# Copyright (c) The GTG Team
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+from unittest import TestCase
+
+from GTG.gtk.browser.quick_add import parse
+from GTG.core.dates import Date
+
+
+class TestQuickAddParse(TestCase):
+
+    def test_empty(self):
+        default_dict = {
+            'title': '',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        self.assertEqual(default_dict, parse(''))
+
+
+    def test_basic_title(self):
+        expected = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        self.assertEqual(expected, parse('Do a thing'))
+
+    def test_basic_tags(self):
+        expected = {
+            'title': 'Do a thing',
+            'tags': set(('home', 'work')),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        text1 = 'Do a thing tags:home,work'
+        text2 = 'Do a thing tags: home,work'
+        text3 = 'Do a thing tag: home,work'
+        text4 = 'Do a thing tag:home,work'
+
+        self.assertEqual(expected, parse(text1))
+        self.assertEqual(expected, parse(text2))
+        self.assertEqual(expected, parse(text3))
+        self.assertEqual(expected, parse(text4))
+
+
+    def test_strip_at(self):
+        expected1 = {
+            'title': 'Do a thing',
+            'tags': set(('home', 'work')),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        expected2 = {
+            'title': 'Do a thing',
+            'tags': set(('家', '仕事')),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        text1 = 'Do a thing tags:@home,@work'
+        text2 = 'Do a thing tags:@家,@仕事'
+
+        self.assertEqual(expected1, parse(text1))
+        self.assertEqual(expected2, parse(text2))
+
+
+    def test_tags_in_title(self):
+        expected1 = {
+            'title': '@phone Maruk about @work',
+            'tags': set(('phone', 'work')),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        expected2 = {
+            'title': '@phone Maruk about @work',
+            'tags': set(('phone', 'work', 'fun')),
+            'start': None,
+            'due': None,
+            'recurring': None
+        }
+
+        text1 = '@phone Maruk about @work'
+        text2 = '@phone Maruk about @work tags: fun'
+
+        self.assertEqual(expected1, parse(text1))
+        self.assertEqual(expected2, parse(text2))
+
+
+    def test_start(self):
+        expected1 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': Date.parse('monday'),
+            'due': None,
+            'recurring': None
+        }
+
+        expected2 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': Date.parse('someday'),
+            'due': None,
+            'recurring': None
+        }
+
+        expected3 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': Date.parse('2099/02/12'),
+            'due': None,
+            'recurring': None
+        }
+
+        text1 = 'Do a thing start:monday'
+        text2 = 'Do a thing starts:monday'
+        text3 = 'Do a thing start: monday'
+        text4 = 'Do a thing defer: someday'
+        text5 = 'Do a thing start: 2099/02/12'
+
+        self.assertEqual(expected1, parse(text1))
+        self.assertEqual(expected1, parse(text2))
+        self.assertEqual(expected1, parse(text3))
+        self.assertEqual(expected3, parse(text5))
+
+
+    def test_due(self):
+        expected1 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': Date.parse('monday'),
+            'recurring': None
+        }
+
+        expected2 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': Date.parse('someday'),
+            'recurring': None
+        }
+
+        expected3 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': Date.parse('2099/02/12'),
+            'recurring': None
+        }
+
+        text1 = 'Do a thing due:monday'
+        text2 = 'Do a thing due:monday'
+        text3 = 'Do a thing due: monday'
+        text4 = 'Do a thing due: someday'
+        text5 = 'Do a thing due: 2099/02/12'
+
+        self.assertEqual(expected1, parse(text1))
+        self.assertEqual(expected1, parse(text2))
+        self.assertEqual(expected1, parse(text3))
+        self.assertEqual(expected3, parse(text5))
+
+
+    def test_repeat(self):
+        expected1 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': 'other-day'
+        }
+
+        expected2 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': 'month'
+        }
+
+        expected3 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': 'monday'
+        }
+
+        expected4 = {
+            'title': 'Do a thing',
+            'tags': set(),
+            'start': None,
+            'due': None,
+            'recurring': 'year'
+        }
+
+        text1 = 'Do a thing every: other-day'
+        text2 = 'Do a thing every:other-day'
+        text3 = 'Do a thing every:month'
+        text4 = 'Do a thing every:monday'
+        text5 = 'Do a thing every:year'
+
+        self.assertEqual(expected1, parse(text1))
+        self.assertEqual(expected1, parse(text2))
+        self.assertEqual(expected2, parse(text3))
+        self.assertEqual(expected4, parse(text5))
