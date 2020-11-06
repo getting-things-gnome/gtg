4c12ef9f1090d94067717dd8543363136c49a60f
zeddo123 <m.drissi@protonmail.com>
2020-11-06 18:54:02 +0100

    Add preference menu for Gamify

--- GTG/plugins/gamify/gamify.py
+++ GTG/plugins/gamify/gamify.py
@@ -8,14 +8,32 @@ from gettext import gettext as _
 
 class Gamify:
     PLUGIN_PATH = os.path.dirname(os.path.abspath(__file__))
+    PLUGIN_NAMESPACE = 'gamify'
+    DEFAULT_PREFERENCES = {
+        "target": 3
+    }
 
     def __init__(self):
-        self.task_target = 0
+        self.configureable = True
 
+    def _init_dialog_pref(self):
         self.builder = Gtk.Builder()
         path = f"{self.PLUGIN_PATH}/prefs.ui"
         self.builder.add_from_file(path)
-        self.pref_dialog = self.builder.get_object('gamify-pref-window')
+        # Get the dialog widget
+        self.pref_dialog = self.builder.get_object('gamify-pref-dialog')
+        # Get the buttonSpin
+        self.spinner = self.builder.get_object('target-spin-button')
+
+        if self.pref_dialog is None or self.spinner is None:
+            raise ValueError('Cannot load preference dialog widget')
+
+        SIGNALS = {
+            "on_preferences_changed": self.on_preferences_changed,
+            "on_dialog_close": self.on_preferences_closed
+        }
+        self.builder.connect_signals(SIGNALS) 
+
 
     def activate(self, plugin_api):
         self.plugin_api = plugin_api
@@ -26,6 +44,13 @@ class Gamify:
         self.menu_item.set_label(_("Gamify"))
         self.menu_item.connect("clicked", self.on_marked_as_done, plugin_api)
         self.plugin_api.add_menu_item(self.menu_item)
+
+        # Init the preference dialog
+        try:
+            self._init_dialog_pref()
+        except ValueError:
+            self.configureable = False
+            log.debug('Cannot load preference dialog widget')
 
         # Connect to the signals
         self.connect_id = self.browser.connect("task-marked-as-done", self.on_marked_as_done)
@@ -42,5 +67,39 @@ class Gamify:
     def is_configurable(self):
         return True
 
+    def preferences_load(self):
+        return self.plugin_api.load_configuration_object(
+            self.PLUGIN_NAMESPACE, "preferences",
+            default_values=self.DEFAULT_PREFERENCES
+        )
+
+    def save_preferences(self, preferences):
+        self.plugin_api.save_configuration_object(
+            self.PLUGIN_NAMESPACE, 
+            "preferences", 
+            preferences
+        )
+
     def configure_dialog(self, manager_dialog):
-        pass
+        if not self.configureable:
+            log.debug('trying to open preference menu, but dialog widget not loaded')
+            return
+
+        preferences = self.preferences_load()
+        self.pref_dialog.set_transient_for(manager_dialog) 
+        
+        self.spinner.set_value(preferences['target'])
+
+        self.pref_dialog.show_all()
+
+    def on_preferences_closed(self, widget=None, data=None):
+        self.pref_dialog.hide()
+        return True
+
+    def on_preferences_changed(self, widget=None, data=None):
+        preferences = self.preferences_load()
+
+        # Get the new preferences
+        preferences['target'] = self.spinner.get_value()
+        
+        self.save_preferences(preferences) 
