3511107412f86ee5c14ad7d0e741ec06c6414e56
zeddo123 <m.drissi@protonmail.com>
2020-11-08 00:06:08 +0100

    Add analitics file
    This file helps us track how many tasks have been done during the day

--- GTG/plugins/gamify/gamify.py
+++ GTG/plugins/gamify/gamify.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 
 from gi.repository import Gio
 from gi.repository import Gtk
@@ -9,6 +10,13 @@ from gettext import gettext as _
 class Gamify:
     PLUGIN_PATH = os.path.dirname(os.path.abspath(__file__))
     PLUGIN_NAMESPACE = 'gamify'
+    DEFAULT_ANALITICS = {
+        "last_task_date": date.today(), # The date of the last task marked as done
+        "last_task_number": 0, # The number of tasks done today
+        "streak": 0, # The number of days in which the goal was achieved
+        "goal_achieved": False, # achieved today's goal
+        "score": 0
+    }
     DEFAULT_PREFERENCES = {
         "target": 3
     }
@@ -23,6 +31,8 @@ class Gamify:
         self.menu = None
         self.stack = None
         self.submenu = None
+
+        self.data = None
 
     def _init_dialog_pref(self):
         # Get the dialog widget
@@ -50,7 +60,6 @@ class Gamify:
 
     def add_menu_enty(self):
         self.menu_item = self.builder.get_object('gamify-entry')
-        self.menu_item.connect("clicked", self.on_marked_as_done, self.plugin_api)
         self.plugin_api.add_menu_item(self.menu_item)
 
     def remove_menu_entry(self):
@@ -74,8 +83,27 @@ class Gamify:
         # Connect to the signals
         self.connect_id = self.browser.connect("task-marked-as-done", self.on_marked_as_done)
 
+        self.data = self.analitics_load()
+        preferences = self.preferences_load()
+        self.update_date(preferences)
+        self.update_streak(preferences)
+        self.analitics_save(self.data)
+        print(self.data)
+
     def on_marked_as_done(self, sender, task_id):
         log.debug('a task has been marked as done')
+        self.data = self.analitics_load()
+        preferences = self.preferences_load()
+
+        # Update the date, if it is different from today
+        self.update_date(preferences)
+
+        # Increase the number of tasks done and update the streak
+        # if the target number of tasks was achieved
+        self.data['last_task_number'] += 1
+        self.update_streak(preferences)
+        self.analitics_save(self.data)
+        print(self.data)
 
     def OnTaskOpened(self, plugin_api):
         pass
@@ -99,6 +127,34 @@ class Gamify:
             "preferences", 
             preferences
         )
+
+    def analitics_load(self):
+        return self.plugin_api.load_configuration_object(
+            self.PLUGIN_NAMESPACE, "analitics",
+            default_values=self.DEFAULT_ANALITICS
+        )
+
+    def analitics_save(self, analitics):
+        self.plugin_api.save_configuration_object(
+            self.PLUGIN_NAMESPACE,
+            "analitics",
+            analitics
+        )
+
+    def update_streak(self, preferences):
+        if self.data['last_task_number'] >= preferences['target']:
+            if not self.data['goal_achieved']:
+                self.data['goal_achieved'] = True
+                self.data['streak'] += 1
+
+    def update_date(self, preferences):
+        today = date.today()
+        if self.data['last_task_date'] != today:
+            if self.data['last_task_number'] < preferences['target']:
+                self.data['streak'] = 0
+            self.data['goal_achieved'] = False
+            self.data['last_task_number'] = 0
+            self.data['last_task_date'] = today
 
     def configure_dialog(self, manager_dialog):
         if not self.configureable:
