6bdbd2fea2f3caa9667abbab9e15e60553188a7d
Diego Garcia Gangl <dnicolas@gmail.com>
2020-12-06 20:16:41 -0300

    Update tag adding for new format

--- GTG/core/task.py
+++ GTG/core/task.py
@@ -813,24 +813,17 @@ class Task(TreeNode):
         if self.tag_added(tagname):
             c = self.content
 
-            # strip <content>...</content> tags
-            if c.startswith('<content>'):
-                c = c[len('<content>'):]
-            if c.endswith('</content>'):
-                c = c[:-len('</content>')]
-
             if not c:
                 # don't need a separator if it's the only text
                 sep = ''
-            elif c.startswith('<tag>'):
+            elif c.startswith('@'):
                 # if content starts with a tag, make a comma-separated list
                 sep = ', '
             else:
                 # other text at the beginning, so put the tag on its own line
                 sep = '\n\n'
 
-            self.content = "<content><tag>%s</tag>%s%s</content>" % (
-                html.escape(tagname), sep, c)
+            self.content = f'{html.escape(tagname)}{sep}{c}'
             # we modify the task internal state, thus we have to call for a
             # sync
             self.sync()
