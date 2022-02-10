c763800f1334f19779396a26dc2f3feb7308c42c
Neui <neuisen@googlemail.com>
2022-02-10 23:41:50 +0100

    Improve plugin error messages english

--- GTG/core/plugins/__init__.py
+++ GTG/core/plugins/__init__.py
@@ -25,16 +25,16 @@ from gettext import gettext as _
 
 class GnomeConfig():
     CANLOAD = _("Everything necessary to run this plugin is available.")
-    CANNOTLOAD = _("The plugin can not be loaded")
-    miss1 = _("Some python modules are missing")
+    CANNOTLOAD = _("This plugin can not be loaded.")
+    miss1 = _("Some python modules are missing.")
     miss2 = _("Please install the following python modules:")
     MODULEMISSING = f"{miss1} \n{miss2}"
-    dmiss1 = _("Some remote dbus objects are missing.")
+    dmiss1 = _("Some remote D-Bus objects are missing.")
     dmiss2 = _("Please start the following applications:")
     DBUSMISSING = f"{dmiss1} \n{dmiss2}"
-    bmiss1 = _("Some modules and remote dbus objects are missing.")
+    bmiss1 = _("Some modules and remote D-Bus objects are missing.")
     bmiss2 = _("Please install or start the following components:")
     MODULANDDBUS = f"{bmiss1} \n{bmiss2}"
-    umiss1 = _("Unknown error while loading the plugin.")
+    umiss1 = _("An unknown error occurred while loading the plugin.")
     umiss2 = _("Very helpful message, isn't it? Please report a bug.")
     UNKNOWN = f"{umiss1} \n{umiss2}"
