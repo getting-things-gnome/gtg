b1bbf76a8599a486da13833704c9989d39abc689
Diego Garcia Gangl <dnicolas@gmail.com>
2022-06-10 20:44:48 -0300

    Remove saved search parenting
    This was a side-effect of saved searches being "special tags". There's
    no good reason to keep around. Users aren't going to use dozens of
    saved searches that need to be organized.  Just having a flat view is
    good enough and saves a ton of complexity.

--- GTG/core/saved_searches.py
+++ GTG/core/saved_searches.py
@@ -36,17 +36,13 @@ class SavedSearch(GObject.Object):
     """A saved search."""
 
     __gtype_name__ = 'gtg_SavedSearch'
-    __slots__ = ['id', 'name', 'query', 'icon', 'children']
 
 
     def __init__(self, id: UUID, name: str, query: str) -> None:
         self.id = id
         self.name = name
         self.query = query
-
         self.icon = None
-        self.children = []
-        self.parent = None
 
 
     def __str__(self) -> str:
@@ -109,41 +105,16 @@ class SavedSearchStore(BaseStore):
             log.debug('Added %s', search)
 
 
-        for element in elements:
-            parent_name = element.get('parent')
-
-            if parent_name and parent_name != 'search':
-                tid = element.get('id')
-
-                parent = self.find(parent_name)
-
-                if parent:
-                    self.parent(tid, parent.id)
-                    log.debug('Added %s as child of %s', element, parent)
-
-
     def to_xml(self) -> Element:
         """Save searches to an LXML element."""
 
         root = Element('searchlist')
-
-        parent_map = {}
-
-        for search in self.data:
-            for child in search.children:
-                parent_map[child.id] = search.name
 
         for search in self.lookup.values():
             element = SubElement(root, self.XML_TAG)
             element.set('id', str(search.id))
             element.set('name', search.name)
             element.set('query', search.query)
-
-            try:
-                element.set('parent', str(parent_map[search.id]))
-            except KeyError:
-                # Toplevel search
-                pass
 
         return root
 
@@ -154,10 +125,7 @@ class SavedSearchStore(BaseStore):
         search_id = uuid4()
         search = SavedSearch(id=search_id, name=name, query=query)
 
-        if parent:
-            self.add(search, parent)
-        else:
-            self.data.append(search)
-            self.lookup[search_id] = search
+        self.data.append(search)
+        self.lookup[search_id] = search
 
         return search
