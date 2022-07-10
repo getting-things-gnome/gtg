25ba5d5450dbcd2853fe406b6951b9949d5832d6
Diego Garcia Gangl <dnicolas@gmail.com>
2022-07-09 22:56:38 -0300

    Update modify tags dialog for new core

--- GTG/gtk/browser/modify_tags.py
+++ GTG/gtk/browser/modify_tags.py
@@ -35,10 +35,9 @@ class ModifyTagsDialog(Gtk.Dialog):
     _tag_entry = Gtk.Template.Child()
     _apply_to_subtasks_check = Gtk.Template.Child()
 
-    def __init__(self, tag_completion, req, app):
+    def __init__(self, tag_completion, app):
         super().__init__()
 
-        self.req = req
         self.app = app
         self.tasks = []
 
@@ -48,9 +47,31 @@ class ModifyTagsDialog(Gtk.Dialog):
         self.last_tag_entry = _("NewTag")
         self.last_apply_to_subtasks = False
 
+
+    def parse_tag_list(self, text):
+        """ Parse a line of a list of tasks. User can specify if the tag is
+        positive or not by prepending '!'.
+
+        @param  text:  string entry from user
+        @return: list of tupples (tag, is_positive)
+        """
+
+        result = []
+        for tag in text.split():
+            if tag.startswith('!'):
+                tag = tag[1:]
+                is_positive = False
+            else:
+                is_positive = True
+
+            result.append((tag, is_positive))
+        return result
+
+
     def modify_tags(self, tasks):
         """ Show and run dialog for selected tasks """
-        if len(tasks) == 0:
+
+        if not tasks:
             return
 
         self.tasks = tasks
@@ -61,54 +82,36 @@ class ModifyTagsDialog(Gtk.Dialog):
 
         self.show()
 
-        self.tasks = []
-
     @Gtk.Template.Callback()
     def on_response(self, widget, response):
         if response == Gtk.ResponseType.APPLY:
             self.apply_changes()
+
         self.hide()
 
     def apply_changes(self):
         """ Apply changes """
-        tags = parse_tag_list(self._tag_entry.get_text())
+
+        tags = self.parse_tag_list(self._tag_entry.get_text())
 
         # If the checkbox is checked, find all subtasks
         if self._apply_to_subtasks_check.get_active():
-            for task_id in self.tasks:
-                task = self.req.get_task(task_id)
-                # FIXME: Python not reinitialize the default value of its
-                # parameter therefore it must be done manually. This function
-                # should be refractored # as far it is marked as depricated
-                for subtask in task.get_subtasks():
-                    subtask_id = subtask.get_id()
-                    if subtask_id not in self.tasks:
-                        self.tasks.append(subtask_id)
+            for task in self.tasks:
+                for subtask in task.children:
+                    if subtask not in self.tasks:
+                        self.tasks.append(subtask)
 
-        for task_id in self.tasks:
-            task = self.req.get_task(task_id)
-            for tag, is_positive in tags:
-                if is_positive:
-                    task.add_tag(tag)
-                else:
-                    task.remove_tag(tag)
-            task.sync()
-
-        # TODO: New Core
-        for tid in self.tasks:
-            t = self.app.ds.tasks.get(tid)
-
+        for task in self.tasks:
             for tag, is_positive in tags:
                 _tag = self.app.ds.tags.new(tag)
 
                 if is_positive:
-                    t.add_tag(_tag)
+                    task.add_tag(_tag)
                 else:
-                    t.remove_tag(_tag)
+                    task.remove_tag(_tag)
 
         self.app.ds.save()
 
         # Rember the last actions
         self.last_tag_entry = self._tag_entry.get_text()
         self.last_apply_to_subtasks = self._apply_to_subtasks_check.get_active()
-# -----------------------------------------------------------------------------
