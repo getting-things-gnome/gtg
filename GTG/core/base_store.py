27d73fb975d512504bb5d9d97289781b19b21af7
Wiesław Młynarski <wieslaw.mlynarski@gmail.com>
2023-02-12 19:37:10 +0100

    Fix warning during running tests

--- GTG/core/base_store.py
+++ GTG/core/base_store.py
@@ -59,7 +59,7 @@ class BaseStore(GObject.Object):
         """Add an existing item to the store."""
 
         if item.id in self.lookup.keys():
-            log.warn('Failed to add item with id %s, already added!',
+            log.warning('Failed to add item with id %s, already added!',
                      item.id)
 
             raise KeyError
@@ -70,7 +70,7 @@ class BaseStore(GObject.Object):
                 item.parent = self.lookup[parent_id]
 
             except KeyError:
-                log.warn(('Failed to add item with id %s to parent %s, '
+                log.warning(('Failed to add item with id %s to parent %s, '
                          'parent not found!'), item.id, parent_id)
                 raise
 
