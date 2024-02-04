b44abf683b7b6cd405b6c6aee90b1d4f06b557bd
Diego Garcia Gangl <dnicolas@gmail.com>
2024-02-03 23:29:10 -0300

    Apply tags when searching and changing tags

--- GTG/gtk/browser/task_pane.py
+++ GTG/gtk/browser/task_pane.py
@@ -274,24 +274,26 @@ class TaskPane(Gtk.ScrolledWindow):
         """Change tasks filter."""
 
         if self.searching:
-            self.searching = False
-            self.filtered.set_filter(self.task_filter)
+            self.search_filter.tags = tags
+            self.search_filter.changed(Gtk.FilterChange.DIFFERENT)
+        else:
+            self.task_filter.tags = tags
+            self.task_filter.no_tags = False
+            self.task_filter.changed(Gtk.FilterChange.DIFFERENT)
 
-        self.task_filter.tags = tags
-        self.task_filter.no_tags = False
-        self.task_filter.changed(Gtk.FilterChange.DIFFERENT)
         self.set_title()
 
     def set_filter_notags(self, tags=[]) -> None:
         """Change tasks filter."""
 
         if self.searching:
-            self.searching = False
-            self.filtered.set_filter(self.task_filter)
+            self.search_filter.tags = []
+            self.search_filter.changed(Gtk.FilterChange.DIFFERENT)
+        else:
+            self.task_filter.tags = []
+            self.task_filter.no_tags = True
+            self.task_filter.changed(Gtk.FilterChange.DIFFERENT)
 
-        self.task_filter.tags = []
-        self.task_filter.no_tags = True
-        self.task_filter.changed(Gtk.FilterChange.DIFFERENT)
         self.set_title()
 
 
