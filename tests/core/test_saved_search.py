6383e5a39430b60e52027682134400af0b662444
Sebastian Grabowski <sebastian@grabel.de>
2024-03-07 11:25:21 +0100

    Fix saved searches tests
    Remove tree testing since Saved searches are not representing
    hierarchies anymore.

--- tests/core/test_saved_search.py
+++ tests/core/test_saved_search.py
@@ -57,19 +57,14 @@ class TestSavedSearch(TestCase):
 
 
     def test_count(self):
-
         store = SavedSearchStore()
 
-        root_1 = store.new('Root search', '@tag')
-        root_2 = store.new('Root search 2', '@tag2')
-        child_2 = store.new('Child search 2', '@another_tag', root_2.id)
+        search = store.new('Root search', '@tag')
+        store.new('Root search 2', '@tag2')
 
-        self.assertEqual(store.count(), 3)
-        self.assertEqual(store.count(root_only=True), 2)
-
-        store.remove(child_2.id)
         self.assertEqual(store.count(), 2)
-        self.assertEqual(store.count(root_only=True), 2)
+        store.remove(search.id)
+        self.assertEqual(store.count(), 1)
 
 
     def test_xml_load_simple(self):
@@ -100,75 +95,17 @@ class TestSavedSearch(TestCase):
 
         store.from_xml(xml_doc)
         self.assertEqual(store.count(), 4)
-        self.assertEqual(store.count(root_only=True), 2)
 
         self.assertEqual(store.lookup['4796b97b-3690-4e74-a056-4153061958df'].query, 'urgent')
-        self.assertEqual(len(store.lookup['2ff11525-a209-4cd9-8f50-859592f1ee37'].children), 1)
 
 
     def test_xml_write_simple(self):
 
         store = SavedSearchStore()
-        search1 = store.new('Some @tag', 'Looking for some tag')
-        search2 = store.new('Some @other @tag', 'Looking for more')
+        search1 = store.add(SavedSearch.create('Some @tag', 'Looking for some tag'))
+        search2 = store.add(SavedSearch.create('Some @other @tag', 'Looking for more'))
 
         xml_root = store.to_xml()
 
         self.assertEqual(len(xml_root), 2)
 
-
-    def test_xml_write_tree(self):
-
-        store = SavedSearchStore()
-        search1 = store.new('Some @tag', 'Looking for some tag')
-        search2 = store.new('Some @other @tag', 'Looking for more')
-
-        xml_root = store.to_xml()
-
-        self.assertEqual(len(xml_root), 2)
-
-
-    def test_parent(self):
-
-        store = SavedSearchStore()
-        search1 = store.new('Some @tag', 'Looking for some tag')
-        search2 = store.new('Some @other @tag', 'Looking for more')
-
-        self.assertEqual(len(store.lookup), 2)
-        self.assertEqual(len(store.data), 2)
-
-        store.parent(search1.id, search2.id)
-
-        self.assertEqual(len(store.data), 1)
-        self.assertEqual(len(search2.children), 1)
-        self.assertEqual(len(store.lookup), 2)
-
-        search3 = store.new('Some @other @tag', 'Looking for more')
-        store.parent(search3.id, search1.id)
-
-        self.assertEqual(len(store.data), 1)
-        self.assertEqual(len(search1.children), 1)
-        self.assertEqual(len(search2.children), 1)
-        self.assertEqual(len(store.lookup), 3)
-
-
-    def test_unparent(self):
-
-        store = SavedSearchStore()
-        search1 = store.new('Some @tag', 'Looking for some tag')
-        search2 = store.new('Some @other @tag', 'Looking for more')
-
-        self.assertEqual(len(store.lookup), 2)
-        self.assertEqual(len(store.data), 2)
-
-        store.parent(search1.id, search2.id)
-
-        self.assertEqual(len(store.data), 1)
-        self.assertEqual(len(search2.children), 1)
-        self.assertEqual(len(store.lookup), 2)
-
-        store.unparent(search1.id, search2.id)
-
-        self.assertEqual(len(store.data), 2)
-        self.assertEqual(len(search2.children), 0)
-        self.assertEqual(len(store.lookup), 2)
